8b07624fcf17d39ef85c0803eb63bc8a50d05c19
Jaehyun Kevin Lee <angle0918@naver.com>
2025-03-28 16:38:03 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 16.40 ms, Memory: 7.35 MB -BaekjoonHub

--- Python3/프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
+++ Python3/프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
@@ -1,10 +1,1 @@
-str = input()
-result = ""
-
-for i in range(len(str)):
-    if str[i].isupper():
-        result += str[i].lower()
-    else:
-        result += str[i].upper()
-        
-print(result)
+print(input().swapcase())
