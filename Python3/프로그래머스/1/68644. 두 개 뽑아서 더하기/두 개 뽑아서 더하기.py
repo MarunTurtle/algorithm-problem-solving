67c9ce9aa520ffbe63a8602b568b7eae0db4ab8b
Jaehyun Kevin Lee <angle0918@naver.com>
2025-05-18 17:17:03 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.43 ms, Memory: 9.24 MB -BaekjoonHub

--- Python3/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ Python3/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,10 +1,21 @@
-def solution(numbers):
-    sum = []
-    length = len(numbers)
-    for i in range(length):
-        for j in range(i + 1, length):
-            sum.append(numbers[i] + numbers[j])
+# def solution(numbers):
+#     sum = []
+#     length = len(numbers)
+#     for i in range(length):
+#         for j in range(i + 1, length):
+#             sum.append(numbers[i] + numbers[j])
             
-    answer = sorted(set(sum))
+#     answer = sorted(set(sum))
     
-    return answer
+#     return answer
+
+def solution(numbers):
+    return sorted(set(numbers[i] + numbers[j] for i in range(len(numbers)) for j in range(i+1, len(numbers))))
+
+"""
+
+def solution(numberes):
+    return sorted(set(numbers[i] + numbers[j] for i in range(len(numbers)) for j in range(i+1, len(numbers))))
+
+
+"""
