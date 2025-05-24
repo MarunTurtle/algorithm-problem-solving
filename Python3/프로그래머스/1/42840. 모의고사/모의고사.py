7cebb9c13c29d6b93fd3e8756798733a3e267176
Jaehyun Kevin Lee <angle0918@naver.com>
2025-05-24 17:23:38 +0900

    [level 1] Title: 모의고사, Time: 6.00 ms, Memory: 9.26 MB -BaekjoonHub

--- Python3/프로그래머스/1/42840. 모의고사/모의고사.py
+++ Python3/프로그래머스/1/42840. 모의고사/모의고사.py
@@ -6,37 +6,40 @@ def solution(answers):
         [2, 1, 2, 3, 2, 4, 2, 5],
         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     ]
-    
-    # 문제 수 선언
-    num_problems = len(answers)
-    
-    # 수포자 별 정답 갯수
-    num_answers = [0] * 3
-    
-    # 수포자 별 정답 맞춰보기
-    for i in range(3):
-        
-        # 수포자 패턴의 길이
-        p_len = len(patterns[i])
-        
-        # 정답 수 파악
-        ans = 0
-        
-        # 문제 배열 내 모든 정답 확인
-        for j in range(num_problems):
-            # n번째 문제의 비교 정답 위치 계산
-            k = (j % p_len)
 
-            # 만약 j번째 문제와 i번째 학생의 k번째 정답과 일치한다면
-            if answers[j] == patterns[i][k]:
-                # 정답 수 하나 증가
-                ans += 1
-        # 최종 정답 수 저장
-        num_answers[i] = ans
+    # 수포자 별 정답 갯수
+    scores = [0] * 3
+    
+    
+    for i, answer in enumerate(answers):
+        for j, pattern in enumerate(patterns):
+            if answer == pattern[i % len(pattern)]:
+                scores[j] += 1
+        
+#     # 수포자 별 정답 맞춰보기
+#     for i in range(3):
+        
+#         # 수포자 패턴의 길이
+#         p_len = len(patterns[i])
+        
+#         # 정답 수 파악
+#         ans = 0
+        
+#         # 문제 배열 내 모든 정답 확인
+#         for j in range(num_problems):
+#             # n번째 문제의 비교 정답 위치 계산
+#             k = (j % p_len)
+
+#             # 만약 j번째 문제와 i번째 학생의 k번째 정답과 일치한다면
+#             if answers[j] == patterns[i][k]:
+#                 # 정답 수 하나 증가
+#                 ans += 1
+#         # 최종 정답 수 저장
+#         num_answers[i] = ans
         
     # 최고 점 찾기
-    max_num = max(num_answers)
+    max_score = max(scores)
     
-    answer = [i + 1 for i in range(3) if num_answers[i] >= max_num]
+    answer = [i + 1 for i in range(3) if scores[i] == max_score]
         
     return answer
