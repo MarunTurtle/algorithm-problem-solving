7cfbd6934888ae771c2b44a6f677f4fdf2ba52ad
Jaehyun Kevin Lee <angle0918@naver.com>
2025-06-15 15:25:56 +0900

    [level 2] Title: 행렬의 곱셈, Time: 143.45 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ Python3/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,14 +1,13 @@
 def solution(arr1, arr2):
+    row_cnt = len(arr1)
+    col_cnt = len(arr2[0])
+    denom_cnt = len(arr2)
     
-    row_count = len(arr1)
-    col_count = len(arr2[0])
-    denom = len(arr2)    
+    answer = [[0] * col_cnt for _ in range(row_cnt)]
     
-    answer = [[0] * col_count for _ in range(row_count)]
-    
-    for i in range(row_count):
-        for j in range(col_count):
-            for k in range(denom):
+    for i in range(row_cnt):
+        for j in range(col_cnt):
+            for k in range(denom_cnt):
                 answer[i][j] += arr1[i][k] * arr2[k][j]
     
     return answer
