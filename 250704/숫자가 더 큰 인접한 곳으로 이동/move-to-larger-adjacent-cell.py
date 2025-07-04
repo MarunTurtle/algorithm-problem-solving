a0d191a10ad93d3ac3321c121129cb2566ec5da7
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-04 12:54:32 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250704/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 250704/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,35 +1,48 @@
+# 변수 선언 및 입력
 n, r, c = map(int, input().split())
+a = [[0] * (n + 1)]
+for _ in range(n):
+    a.append([0] + list(map(int, input().split())))
 
-a = [[0] * (n + 1) for _ in range(n + 1)]
+# 방문하게 되는 숫자들을 담을 곳입니다.
+visited_nums = []
 
-for i in range(1, n + 1):
-    row = list(map(int, input().split()))
-    for j in range(1, n + 1):
-        a[i][j] = row[j - 1]
+# 범위가 격자 안에 들어가는지 확인합니다. 
+def in_range(x, y):
+    return 1 <= x <= n and 1 <= y <= n
 
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
+# 범위가 격자 안이고, 해당 위치의 값이 더 큰지 확인합니다. 
+def can_go(x, y, curr_num):
+    return in_range(x, y) and a[x][y] > curr_num
 
-num_list = [a[r][c]]
+# 조건에 맞춰 움직여봅니다. 
+# 움직였다면 true를 반환하고
+# 만약 움직일 수 있는 곳이 없었다면 false를 반환합니다. 
+def simulate():
+    global r, c
+
+    drs, dcs = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+    for dr, dc in zip(drs, dcs):
+        nr, nc = r + dr, c + dc
+
+        if can_go(nr, nc, a[r][c]):
+            r, c = nr, nc
+            return True
+
+    return False
+
+# 초기 위치를 방문 위치에 추가
+visited_nums.append(a[r][c])
 
 while True:
-    moved = False
-    cur_num = a[r][c]
-
-    for i in range(4):
-        nr = r + dr[i]
-        nc = c + dc[i]
-
-        if 1 <= nr <= n and 1 <= nc <= n:        
-            if a[nr][nc] > cur_num:
-                r, c = nr, nc
-                num_list.append(a[nr][nc])
-                moved = True
-                break
-
-    if not moved:
+    # 조건에 맞춰 움직여 봅니다. 
+    greater_number_exist = simulate()
+    # 인접한 곳에 더 큰 숫자가 없다면 종료
+    if not greater_number_exist:
         break
-        
-print(*num_list)    
+    # 움직이고 난 후의 위치를 방문 위치에 넣기
+    visited_nums.append(a[r][c])
 
-    
+# 출력
+print(*visited_nums)
