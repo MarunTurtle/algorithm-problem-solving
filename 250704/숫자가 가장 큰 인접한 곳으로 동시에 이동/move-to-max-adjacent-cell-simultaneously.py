7f6313faf1a54e1d0a24661b8926312444970525
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-04 17:34:41 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 86ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250704/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 250704/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -1,59 +1,36 @@
-# from collections import Counter
+from collections import defaultdict
 
-# N 격자, M 구슬 갯수, T 시간 
+# 입력 받기
 n, m, t = map(int, input().split())
-
-# Create n x n grid
-a = [list(map(int, input().split())) for _ in range(n)]
-
-# Get m marble positions
+grid = [list(map(int, input().split())) for _ in range(n)]
 marbles = [tuple(map(int, input().split())) for _ in range(m)]
 
-def in_range(x, y):
-    return 1 <= x <= n and 1 <= y <= n
+# 상하좌우 방향 (우선순위: 상 > 하 > 좌 > 우)
+drs = [-1, 1, 0, 0]
+dcs = [0, 0, -1, 1]
 
-# 이동 우선순위 반영 (가장 큰 값 중 상→하→좌→우 순)
-def simulation(r, c):
-    drs, dcs = [-1, 1, 0, 0], [0, 0, -1, 1]  # 상하좌우
-    max_val = a[r-1][c-1]
-
-    # 인접 칸 중 가장 큰 숫자 구하기
-    for dr, dc in zip(drs, dcs):
-        nr, nc = r + dr, c + dc
-        if in_range(nr, nc):
-            max_val = max(max_val, a[nr-1][nc-1])
-
-    # 우선순위에 따라 가장 큰 숫자 칸으로 이동
-    for dr, dc in zip(drs, dcs):
-        nr, nc = r + dr, c + dc
-        if in_range(nr, nc) and a[nr-1][nc-1] == max_val and a[nr-1][nc-1] > a[r-1][c-1]:
-            return (nr, nc)
-
-    return (r, c)
-
-# 중복 확인
-def check_duplicate(marbles):
-    is_duplicate = [False] * len(marbles)
-    for i in range(len(marbles)):
-        for j in range(i+1, len(marbles)):
-            if marbles[i] == marbles[j]:
-                is_duplicate[i] = True
-                is_duplicate[j] = True
-    return is_duplicate
-
-# 중복 제거
-def delete_duplicate(marbles):
-    is_duplicate = check_duplicate(marbles)
-    return [marbles[i] for i in range(len(marbles)) if not is_duplicate[i]]
-    # count = Counter(marbles)
-    # return [pos for pos in marbles if count[pos] == 1]
-
-# 시뮬레이션 T초 진행
+# T초 동안 시뮬레이션 반복
 for _ in range(t):
-    next_marbles = []
+    # 다음 위치 저장용
+    next_pos = defaultdict(int)
+    move_list = []
+
     for r, c in marbles:
-        next_marbles.append(simulation(r, c))
-    marbles = delete_duplicate(next_marbles)
+        max_val = grid[r - 1][c - 1]
+        target = (r, c)  # 이동하지 않을 수도 있음
+
+        for dr, dc in zip(drs, dcs):
+            nr, nc = r + dr, c + dc
+            if 1 <= nr <= n and 1 <= nc <= n:
+                if grid[nr - 1][nc - 1] > max_val:
+                    max_val = grid[nr - 1][nc - 1]
+                    target = (nr, nc)
+
+        move_list.append(target)
+        next_pos[target] += 1
+
+    # 한 칸에 여러 개 구슬 모이면 모두 제거
+    marbles = [pos for pos in move_list if next_pos[pos] == 1]
 
 # 결과 출력
 print(len(marbles))
