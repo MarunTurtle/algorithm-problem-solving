b85cbe59c862caf6f288ebc59af9c85f820d59be
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-04 17:40:48 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 76ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250704/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 250704/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -1,36 +1,46 @@
-from collections import defaultdict
+# 상하좌우 이동
+drs = [-1, 1, 0, 0]
+dcs = [0, 0, -1, 1]
+
+def in_range(n, r, c):
+    return 0 <= r < n and 0 <= c < n
+
+def simulate(n, m, t, grid, marbles):
+    count = [[0] * n for _ in range(n)]
+    
+    # 초기 구슬 위치 설정
+    for r, c in marbles:
+        count[r][c] += 1
+
+    for _ in range(t):
+        next_count = [[0] * n for _ in range(n)]
+        
+        for r in range(n):
+            for c in range(n):
+                if count[r][c] > 0:
+                    max_val = grid[r][c]
+                    next_pos = (r, c)
+                    for dir in range(4):
+                        nr, nc = r + drs[dir], c + dcs[dir]
+                        if in_range(n, nr, nc) and grid[nr][nc] > max_val:
+                            max_val = grid[nr][nc]
+                            next_pos = (nr, nc)
+                    next_count[next_pos[0]][next_pos[1]] += count[r][c]
+        
+        # 충돌 처리
+        for r in range(n):
+            for c in range(n):
+                if next_count[r][c] >= 2:
+                    next_count[r][c] = 0
+        count = next_count
+
+    # 살아남은 구슬 수 세기
+    return sum(count[r][c] for r in range(n) for c in range(n))
 
 # 입력 받기
 n, m, t = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
-marbles = [tuple(map(int, input().split())) for _ in range(m)]
-
-# 상하좌우 방향 (우선순위: 상 > 하 > 좌 > 우)
-drs = [-1, 1, 0, 0]
-dcs = [0, 0, -1, 1]
-
-# T초 동안 시뮬레이션 반복
-for _ in range(t):
-    # 다음 위치 저장용
-    next_pos = defaultdict(int)
-    move_list = []
-
-    for r, c in marbles:
-        max_val = grid[r - 1][c - 1]
-        target = (r, c)  # 이동하지 않을 수도 있음
-
-        for dr, dc in zip(drs, dcs):
-            nr, nc = r + dr, c + dc
-            if 1 <= nr <= n and 1 <= nc <= n:
-                if grid[nr - 1][nc - 1] > max_val:
-                    max_val = grid[nr - 1][nc - 1]
-                    target = (nr, nc)
-
-        move_list.append(target)
-        next_pos[target] += 1
-
-    # 한 칸에 여러 개 구슬 모이면 모두 제거
-    marbles = [pos for pos in move_list if next_pos[pos] == 1]
+marbles = [tuple(map(lambda x: int(x)-1, input().split())) for _ in range(m)]  # 0-indexed로 변환
 
 # 결과 출력
-print(len(marbles))
+print(simulate(n, m, t, grid, marbles))
