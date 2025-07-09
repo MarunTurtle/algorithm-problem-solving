c90c70bbd980fa922c57cb02b6ccd8a6852b8af6
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-09 23:21:28 +0900

    Title: 점수 비교, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250709/점수 비교/score-comparison.py
+++ 250709/점수 비교/score-comparison.py
@@ -1,7 +1,7 @@
 a_eng, a_math = map(int, input().split())
 b_eng, b_math = map(int, input().split())
 
-if a_eng > b_eng and b_eng > b_math:
+if a_eng > b_eng and a_math > b_math:
     print(1)
 else:
     print(0)
