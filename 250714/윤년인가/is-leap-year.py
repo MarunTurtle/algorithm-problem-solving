4352e81432f93bd955bfacc78eb86fc1c5a4e725
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-14 00:10:00 +0900

    Title: 윤년인가, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250714/윤년인가/is-leap-year.py
@@ -0,0 +1,11 @@
+year = int(input())
+
+def is_lunar(n):
+    if (n % 4 == 0):
+        return True
+    elif (n % 100 == 0) and (n % 400 != 0):
+        return True
+    else:
+       return False
+
+print("true") if is_lunar(year) else print("false")
