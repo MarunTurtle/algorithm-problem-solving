f07db27b72002651725a0c674d770df9b5473b74
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-16 01:06:10 +0900

    [Bronze III] Title: 웰컴 키트, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ Python/백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -1,24 +1,13 @@
 n = int(input())
-
 sizes = list(map(int, input().split()))
-
 t, p = map(int, input().split())
 
 def count_t(t, sizes):
-    cnt = 0
-    
-    for size in sizes:
-        if size == 0:
-          continue
-        if size % t == 0:
-          cnt += size // t
-        else:
-          cnt += size // t + 1
-    return cnt
+    return sum((s + t - 1) // t for s in sizes)
 
 def count_p(p, n):
     bundle = n // p
-    rmnder = n - (bundle * p)
+    rmnder = n % p
     return bundle, rmnder
 
 a1 = count_t(t, sizes)
