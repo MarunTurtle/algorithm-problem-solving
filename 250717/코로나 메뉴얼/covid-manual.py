01ec893308aca1a1fa729d874cc1d9ba9b457a07
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-17 22:39:15 +0900

    Title: 코로나 메뉴얼, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250717/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,18 @@
+s1, t1 = map(str, input().split())
+s2, t2 = map(str, input().split())
+s3, t3 = map(str, input().split())
+
+t1 = int(t1)
+t2 = int(t2)
+t3 = int(t3)
+
+patients = [[s1, t1], [s2, t2], [s3, t3]]
+
+for patient in patients:
+    if patient[0] >= 37 and patient[1] == 'Y':
+        cnt += 1
+
+if cnt >= 2:
+    print('E')
+else:
+    print('N')
