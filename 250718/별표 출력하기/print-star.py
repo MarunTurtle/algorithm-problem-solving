49fa13e71cebab86af434838d19a2b0effb9a772
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-18 21:28:37 +0900

    Title: 별표 출력하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250718/별표 출력하기/print-star.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(i+1):
+        print('*', end=" ")
+    print()
+
+for k in range(n-1, 0, -1):
+    for l in range(k):
+        print('*', end=" ")
+    print()
