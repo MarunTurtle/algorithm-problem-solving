ff52ab4a81149a684f668f7423caa63ba1b37e1a
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-19 16:44:57 +0900

    Title: 배열의 평균, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250719/배열의 평균/ave-of-array.py
+++ 250719/배열의 평균/ave-of-array.py
@@ -4,7 +4,7 @@ for i in range(2):
     sum_row = 0
     for j in range(4):
         sum_row += grid[i][j]
-    print(f"{sum_row//4:.1f}", end=" ")
+    print(f"{sum_row/4:.1f}", end=" ")
 
 print()
 
@@ -12,10 +12,11 @@ for j in range(4):
     sum_col = 0
     for i in range(2):
         sum_col += grid[i][j]
-    print(f"{sum_col//2:.1f}", end=" ")
+    print(f"{sum_col/2:.1f}", end=" ")
 print()
+
 sum_all = 0
 for i in range(2):
     for j in range(4):
         sum_all += grid[i][j]
-print(f"{sum_all//8:.1f}")
+print(f"{sum_all/8:.1f}")
