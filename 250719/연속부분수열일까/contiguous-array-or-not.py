8f3d0375028bdad8dbd6444249e42d5b547458ca
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-19 14:50:59 +0900

    Title: 연속부분수열일까, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250719/연속부분수열일까/contiguous-array-or-not.py
+++ 250719/연속부분수열일까/contiguous-array-or-not.py
@@ -4,18 +4,13 @@ b = list(map(int, input().split()))
 
 is_part = False
 
-if n > m:
-    for i in range(n - m):
+if n >= m:
+    for i in range(n - m + 1):
         if a[i:i+m] == b:
             is_part = True
             break
-else:
-    if a == b:
-        is_part = True
-
 
 if is_part:
     print('Yes')
 else:
     print('No')
-
