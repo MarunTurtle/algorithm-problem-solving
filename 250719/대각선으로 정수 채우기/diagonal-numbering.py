e09ea35470983a872896ff123a6eee320ff5216a
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-19 21:02:04 +0900

    Title: 대각선으로 정수 채우기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250719/대각선으로 정수 채우기/diagonal-numbering.py
+++ 250719/대각선으로 정수 채우기/diagonal-numbering.py
@@ -1,12 +1,19 @@
 n, m = map(int, input().split())
-matrix = [[0] * m for _ in range(n)]
+matrix = [[0 for _ in range(m)] for _ in range(n)] 
 
 cnt = 1
-for s in range(n + m - 1):  # s = i + j
-    for i in range(s + 1):
-        j = s - i
-        if 0 <= i < n and 0 <= j < m:
-            matrix[i][j] = cnt
+
+for start_r in range(n):
+    for start_c in range(m):
+        if matrix[start_r][start_c] == 0:
+            curr_r = start_r
+            curr_c = start_c
+        
+        while 0 <= curr_c and curr_r < n:
+            matrix[curr_r][curr_c] = cnt
+            
+            curr_r += 1
+            curr_c -= 1
             cnt += 1
 
 for row in matrix:
