3fb7bb75030e3247bcd3290b249e7481132fc56f
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-19 13:40:39 +0900

    Title: 완전수, Time: 54ms, Memory: 17MB, Status: Passed - Codetree

--- 250719/완전수/perfect-number.py
+++ 250719/완전수/perfect-number.py
@@ -4,13 +4,13 @@ cnt = 0
 # Please write your code here.
 for i in range(start, end + 1):
     
-    nums = []
+    divisble_sum = 0
     
     for j in range(1, i):
         if i % j == 0:
-            nums.append(j)
-    
-    if sum(nums) == i:
+            divisble_sum += j
+        
+    if divisble_sum == i:
         cnt += 1
 
 print(cnt)
