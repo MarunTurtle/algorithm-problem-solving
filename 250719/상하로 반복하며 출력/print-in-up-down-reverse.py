0093415780783bfa698e4f76c2284840600d0c43
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-19 12:18:32 +0900

    Title: 상하로 반복하며 출력, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250719/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+cnt = 1
+
+grid = [[0] * n for _ in range(n)]
+
+for j in range(n):
+    for i in range(n):
+        if j % 2 == 0:
+            grid[i][j] = cnt
+        else:
+            grid[i][j] = n + 1 - cnt
+        if cnt == n:
+                cnt = 1
+        else:
+            cnt += 1
+
+print('\n'.join(''.join(map(str, row)) for row in grid))
