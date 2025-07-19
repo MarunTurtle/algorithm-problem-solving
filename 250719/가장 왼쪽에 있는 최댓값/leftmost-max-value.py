7aab907ebcd9237acd166c4923270ab678bb292a
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-19 15:55:53 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 250719/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 250719/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -1,7 +1,15 @@
 n = int(input())
-a = list(map(int, input().split()))
+a = [0] + list(map(int, input().split()))
 
 # Please write your code here.
-idx = a.index(max(a))
-print(idx+1, end=" ")
-print(a[0:idx].index(max(a[0:idx]))+1)
+max_idx = n + 1
+max_val = 0
+
+while max_idx != 1:
+    max_val = 0
+    for i in range(1, max_idx):
+        if a[i] > max_val:
+            max_val = a[i]
+            max_idx = i
+    print(max_idx, end=" ")
+
