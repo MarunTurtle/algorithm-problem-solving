120198ac34dc516f30e5128bc815e8fbc6744919
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-19 13:58:43 +0900

    Title: 소수 구하기, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

--- 250719/소수 구하기/get-prime.py
+++ 250719/소수 구하기/get-prime.py
@@ -1,11 +1,11 @@
 n = int(input())
 
 is_prime = [True] * (n + 1)
-is_prime[0] = is_primte[1] = False
+is_prime[0] = is_prime[1] = False
 
 for i in range(2, int(n**0.5) + 1):
     if is_prime[i]:
         for j in range(i**2, n+1, i):
             is_prime[j] = False
 
-return count([i for i in range(2, n + 1) if is_prime[i]])
+print(' '.join(f"{i}" for i in range(2, n + 1) if is_prime[i]))
