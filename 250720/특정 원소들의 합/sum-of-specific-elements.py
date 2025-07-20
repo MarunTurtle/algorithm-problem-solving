9ea6c333c4458930c92ccac9c8dfb6a2704c994d
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-20 20:20:13 +0900

    Title: 특정 원소들의 합, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250720/특정 원소들의 합/sum-of-specific-elements.py
+++ 250720/특정 원소들의 합/sum-of-specific-elements.py
@@ -1,9 +1,4 @@
-matrix = [[0] * 4 for _ in range(4)]
-
-for i in range(4):
-    nums = list(map(int, input().split()))
-    for j in range(4):
-        matrix[i][j] = nums[j]
+matrix = [list(map(int, input().split())) for _ in range(4)]
 
 total = 0
 
