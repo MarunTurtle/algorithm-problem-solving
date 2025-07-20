f077e1fc706a4bab9d2d289d83dfd751715dac69
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-20 16:01:29 +0900

    Title: 미는 횟수, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250720/미는 횟수/number-of-pushes.py
+++ 250720/미는 횟수/number-of-pushes.py
@@ -1,10 +1,14 @@
 str1 = input()
 str2 = input()
 cnt = 0
+trg = len(str1)
 
 while True:
-    str1 = str1[-1] + str1[:-1]
-    cnt += 1
     if str1 == str2:
         print(cnt)
+        break
+    str1 = str1[-1] + str1[:-1]
+    cnt += 1
+    if cnt == trg:
+        print(-1)
         break
