8e8914ea08153fd63119d56b51e7f96384e6746c
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-20 20:06:44 +0900

    Title: 나눗셈의 나머지, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250720/나눗셈의 나머지/remainder-of-division.py
+++ 250720/나눗셈의 나머지/remainder-of-division.py
@@ -3,8 +3,7 @@ a, b = map(int, input().split())
 count = [0] * b
 
 while a > 1:
-    rmder = a % b
-    count[rmder] += 1
+    count[a % b] += 1
     a = a // b
 
 total = sum(x ** 2 for x in count)
