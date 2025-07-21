00d0778f716107796bb23c50c10f47193681442f
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-21 22:10:25 +0900

    Title: 사칙연산 함수, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250721/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,27 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+# Please write your code here.
+def get_prod(x, y):
+    return f"{x} * {y} = {x * y}"
+
+def get_sum(x, y):
+    return f"{x} * {y} = {x + y}"
+
+def get_quotient(x, y):
+    return f"{x} * {y} = {x / y}"
+
+def get_diff(x, y):
+    return f"{x} * {y} = {x - y}"
+
+if o == "+":
+    print(get_sum(a, c))
+elif o == '-':
+    print(get_diff(a, c))
+elif o == '*':
+    print(get_prod(a, c))
+elif o == '/':
+    print(get_quotient(a, c))
+else:
+    print(False)
