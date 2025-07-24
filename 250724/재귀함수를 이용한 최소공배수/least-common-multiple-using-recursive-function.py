8408483118d0a0b645f204315cbd738dec3709a1
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-24 21:58:14 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250724/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 250724/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,14 +1,14 @@
-# 입력
 n = int(input())
-arr = list(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
 
 def lcm(a, b):
     gcd = 1
     for i in range(1, min(a, b) + 1):
         if a % i == 0 and b % i == 0:
             gcd = i
-    
+
     return a * b // gcd
+
 
 def get_lcm_all(index):
     if index == 1:
