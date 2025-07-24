f0034aa19c7fa8fc41afb71be3a13c4c661c3a9b
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-24 21:57:14 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 80ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250724/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 250724/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,22 +1,18 @@
-import math
-
-# 최대공약수 (GCD)
-def gcd(a, b):
-    return math.gcd(a, b)
-
-# 최소공배수 (LCM) - 두 수
-def lcm(a, b):
-    return a * b // gcd(a, b)
-
-# 리스트의 LCM (재귀)
-def lcm_list(arr, idx=0):
-    if idx == len(arr) - 1:
-        return arr[idx]
-    return lcm(arr[idx], lcm_list(arr, idx + 1))
-
 # 입력
 n = int(input())
 arr = list(map(int, input().split()))
 
-# 출력
-print(lcm_list(arr))
+def lcm(a, b):
+    gcd = 1
+    for i in range(1, min(a, b) + 1):
+        if a % i == 0 and b % i == 0:
+            gcd = i
+    
+    return a * b // gcd
+
+def get_lcm_all(index):
+    if index == 1:
+        return arr[1]
+    return lcm(get_lcm_all(index - 1), arr[index])
+
+print(get_lcm_all(n))
