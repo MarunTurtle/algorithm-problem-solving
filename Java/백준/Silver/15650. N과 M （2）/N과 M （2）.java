4635ea5daab91a7b81aa80e878bd17cd570bc8e1
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-25 12:04:08 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14272 KB -BaekjoonHub

---
+++ Java/백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        result = new int[M];
+
+        backtrack(1, 0);
+        System.out.println(sb);
+    }
+
+    static void backtrack(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            result[depth] = i;
+            backtrack(i + 1, depth + 1);
+        }
+    }
+}
