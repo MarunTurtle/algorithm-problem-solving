94990e26ee2a8e3afe8e50ca16869e33093c8f71
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-25 10:49:21 +0900

    [Silver III] Title: N과 M (1), Time: 244 ms, Memory: 22320 KB -BaekjoonHub

--- Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,35 +1,42 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
     static int[] result;
     static boolean[] visited;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();  // 1부터 N까지
-        M = sc.nextInt();  // M개 선택
+    static StringBuilder sb = new StringBuilder();
+     
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+    
         result = new int[M];
-        visited = new boolean[N + 1]; // 인덱스 1부터 사용
+        visited = new boolean[N+1];
+        
         dfs(0);
+        
+        System.out.print(sb);
     }
-
+        
     static void dfs(int depth) {
         if (depth == M) {
-            for (int i = 0; i < M; i++) {
-                System.out.print(result[i] + " ");
+            for(int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
             }
-            System.out.println();
+            sb.append('\n');
             return;
         }
-
-        // 1부터 N까지 반복
+        
         for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
-                visited[i] = true;      // 방문 체크
-                result[depth] = i;      // 현재 수열에 추가
-                dfs(depth + 1);         // 다음 자리 탐색
-                visited[i] = false;     // 백트래킹 (되돌리기)
+                visited[i] = true;
+                result[depth] = i;
+                dfs(depth + 1);
+                visited[i] = false;
             }
         }
     }
