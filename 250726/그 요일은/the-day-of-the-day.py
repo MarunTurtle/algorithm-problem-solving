ed0d87ea69b0213fc1d0e0e125ae6be1f5290604
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-26 17:00:32 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250726/그 요일은/the-day-of-the-day.py
+++ 250726/그 요일은/the-day-of-the-day.py
@@ -1,8 +1,8 @@
 m1, d1, m2, d2 = map(int, input().split())
 tgt_weekday = input()
 
-days_in_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 weekday = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+days_in_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 tgt_number = weekday.index(tgt_weekday)
 
@@ -12,12 +12,14 @@ def count_days(m, d):
         ans += days_in_month[i]
     return ans
 
+answer = 0
 start = count_days(m1, d1)
 end = count_days(m2, d2)
-answer = 0
+cur_day = weekday.index('Mon')
 
 for i in range(start, end + 1):  
-    if (i - start) % 7 == tgt_number:  
+    if cur_day == tgt_number:  
         answer += 1
+    cur_day = (cur_day + 1) % 7
 
 print(answer)
