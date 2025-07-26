10f2723d3f14a4bcc27f840ab91012409c8ec26d
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-26 15:21:46 +0900

    Title: 원점으로부터의 거리, Time: 66ms, Memory: 17MB, Status: Passed - Codetree

--- 250726/원점으로부터의 거리/distance-from-origin.py
+++ 250726/원점으로부터의 거리/distance-from-origin.py
@@ -1,23 +1,22 @@
 n = int(input())
 
-class Coord:
-    def __init__(self, x, y, idx):
-        self.x = x
-        self.y = y
-        self.idx = idx
-
 def get_distance(x, y):
-    return abs(x - 0) + abs(y - 0)
+    return abs(x) + abs(y)
+
+# class Coord:
+#     def __init__(self, x, y, idx):
+#         self.x = x
+#         self.y = y
+#         self.idx = idx
 
 coords = []
 for i in range(1, n+1):
     x, y = tuple(map(int, input().split()))
-    idx = i
-    coord = Coord(x, y, idx)
-    coords.append(coord)
+    # idx = i
+    # coord = Coord(x, y, idx)
+    coords.append((get_distance(x, y), i))
 
-coords.sort(key = lambda x: (get_distance(x.x, x.y), x.idx))
+coords.sort()
 
-for coord in coords:
-    print(f'{coord.idx}')
-    
+for _, idx in coords:
+    print(idx)    
