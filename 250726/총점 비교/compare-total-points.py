976312d97107e06fc51c95f97ecf62acd040123e
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-26 14:26:01 +0900

    Title: 총점 비교, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250726/총점 비교/compare-total-points.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self, name, score1, score2, score3):
+        self.name = name
+        self.score1 = score1
+        self.score2 = score2
+        self.score3 = score3
+
+n = int(input())
+students = []
+for _ in range(n):
+    name, score1, score2, score3 = input().split()
+    score1, score2, score3 = int(score1), int(score2), int(score3)
+
+    students.append(Student(name, score1, score2, score3))
+
+students.sort(key=lambda x:(x.score1 + x.score2 + x.score3))
+
+for student in students:
+    print(student.name, student.score1, student.score2, student.score3)    
