45a8fa3b66f7638e1e91c5365cf4b25991d64827
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-29 22:46:10 +0900

    Title: 최대로 겹치는 지점, Time: 78ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250729/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,15 @@
+MAX_R = 100
+
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+checked = [0] * MAX_R
+
+def check_box(x, y):
+    global checked
+    for i in range(x, y+1):
+        checked[i] += 1
+
+for a, b in segments:
+    check_box(a, b)
+
+print(max(checked))
