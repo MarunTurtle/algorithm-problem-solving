aadcafa1835b89d13c359aa578dcaae0884b6e1a
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-29 22:41:15 +0900

    Title: 최대로 겹치는 구간, Time: 73ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250729/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 250729/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,10 +1,13 @@
-n = int(input())
-dots = [0] * 201
+OFFSET = 100
+MAX_R = 200
 
-for i in range(n):
+n = int(input())
+checked = [0] * (MAX_R + 1)
+
+for + in range(n):
     a, b = tuple(map(int, input().split()))
-    a += 100
-    b += 100
+    a += OFFSET
+    b += OFFSET
     for j in range(a, b):
         dots[j] += 1
 
