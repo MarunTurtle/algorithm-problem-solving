fdb8fa375e10185f86caa9926009e94ba7c665b2
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-30 13:59:07 +0900

    Title: 왔다 갔던 구역 2, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 250730/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250730/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,46 +1,23 @@
-OFFSET = 1000
-MAX_R = 2000
+from collections import Counter
 
-# 변수 선언 및 입력
-n = int(input())
-segments = []
+N = int(input())
+pos = 0
+cnt = Counter()
 
-# 현재 위치
-cur = 0
+for _ in range(N):
+    x, d = input().split()
+    x = int(x)
+    if d == 'R':
+        for _ in range(x):
+            seg = (pos, pos + 1)
+            seg = tuple(sorted(seg))
+            cnt[seg] += 1
+            pos += 1
+    else:
+        for _ in range(x):
+            seg = (pos, pos - 1)
+            seg = tuple(sorted(seg))
+            cnt[seg] += 1
+            pos -= 1
 
-for _ in range(n):
-	distance, direction = tuple(input().split())
-	distance = int(distance)
-	
-	if direction == 'L':
-		# 왼쪽으로 이동할 경우 : cur - distance ~ cur까지 경로 이동
-		section_left = cur - distance
-		section_right = cur
-		cur -= distance
-	else:
-		# 오른쪽으로 이동할 경우 : cur ~ cur + distance까지 경로 이동
-		section_left = cur
-		section_right = cur + distance
-		cur += distance
-	
-	segments.append([section_left, section_right])
-
-	
-checked = [0] * (MAX_R + 1)
-
-for x1, x2 in segments:
-	# OFFSET을 더해줍니다.
-	x1, x2 = x1 + OFFSET, x2 + OFFSET
-	
-	# 구간을 칠해줍니다.
-	# 구간 단위로 진행하는 문제이므로
-	# x2에 등호가 들어가지 않음에 유의합니다.
-	for i in range(x1, x2):
-		checked[i] += 1
-
-# 2번 이상 지나간 영역의 크기를 구합니다.
-cnt = 0
-for elem in checked:
-	if elem >= 2:
-		cnt += 1
-print(cnt)
+print(sum(1 for v in cnt.values() if v >= 2))
