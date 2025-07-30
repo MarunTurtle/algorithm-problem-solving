6cc0dfc375481e0dc0247278bd04328ce5696939
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-30 21:10:42 +0900

    Title: 신기한 타일 뒤집기, Time: 92ms, Memory: 21MB, Status: Passed - Codetree

--- 250730/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 250730/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,37 +1,30 @@
 MAX_K = 100000
-# 변수 선언 및 입력:
+
+w, b = 0, 0
+
+checked = [0] * (2 * MAX_K + 1)
+
+cur = 0
+
 n = int(input())
-a = [0] * (2 * MAX_K + 1)
 
-b, w = 0, 0
-
-cur = MAX_K
-for _ in range(n):
-    x, c = tuple(input().split())
+for i in range(n):
+    x, direction = tuple(input().split())
     x = int(x)
 
-    if c == 'L':
-        # x칸 왼쪽으로 칠합니다.
-        while x > 0:
-            a[cur] = 1
-            x -= 1
-
-            if x: 
-                cur -= 1
-    else:
-        # x칸 오른쪽으로 칠합니다.
-        while x > 0:
-            a[cur] = 2
-            x -= 1
-
-            if x: 
-                cur += 1
+    if direction == 'L':
+        for i in range(x):
+            checked[cur - i] = 1
+        cur = cur - x + 1
+    else: 
+        for i in range(x):
+            checked[cur + i] = 2
+        cur = cur + x - 1
 
 for i in range(2 * MAX_K + 1):
-    if a[i] == 1: 
+    if checked[i] == 1:
         w += 1
-    if a[i] == 2: 
+    elif checked[i] == 2:
         b += 1
 
-# 정답을 출력합니다.
 print(w, b)
