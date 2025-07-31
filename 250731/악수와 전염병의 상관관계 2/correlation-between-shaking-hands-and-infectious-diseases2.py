c6fbc0e85211a018e46311c1cc14d706a837d854
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-31 22:44:56 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 54ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250731/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,26 @@
+MAX_T = 250
+# n명의 개발자
+# k번까지 전염
+# p가 전염 시작
+# t번 동안 악수 진행
+n, k, p, t = map(int, input().split())
+
+developers = [0] * (n + 1)
+developers[p] = 1
+cnt = 0
+
+time = [(0, 0)] * (MAX_T + 1)
+
+for i in range(t):
+    s, x, y = map(int, input().split())
+    time[s] = (x, y)
+
+for x, y in time:
+    if x == p or y == p:
+        if cnt < k:
+            developers[x] = 1
+            developers[y] = 1
+            cnt += 1
+
+for i in range(1, n + 1):
+    print(developers[i], end="")
