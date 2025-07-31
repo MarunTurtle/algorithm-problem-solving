629999926e60e9ef316cc0feda8b8659d5a1fe80
Jaehyun Kevin Lee <angle0918@naver.com>
2025-07-31 21:48:18 +0900

    Title: 선두를 지켜라, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250731/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,40 @@
+n, m = map(int, input().split())
+a_loc = []
+b_loc = []
+
+a = 0
+b = 0
+
+for i in range(n):
+    v, t = map(int, input().split())
+    for j in range(t):
+        a += v
+        a_loc.append(a)
+
+for j in range(m):
+    v, t = map(int, input().split())
+    for k in range(t):
+        b += v
+        b_loc.append(b)
+
+cnt = 0
+leader = ''
+
+for i in range(len(b_loc)):
+    cur_a = a_loc[i]
+    cur_b = b_loc[i]
+    cur_leader = leader
+    if cur_a > cur_b:
+        new_leader = 'a'
+    elif cur_a < cur_b:
+        new_leader = 'b'
+    else:
+        new_leader = cur_leader
+
+    if i == 0:
+        leader = new_leader
+    elif new_leader != cur_leader:
+        cnt += 1
+        leader = new_leader
+
+print(cnt)
