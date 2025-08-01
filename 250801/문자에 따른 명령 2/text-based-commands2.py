09f904392142b55b0c6402931e6d1242f84a5b71
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-01 16:11:12 +0900

    Title: 문자에 따른 명령 2, Time: 61ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250801/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,23 @@
+x, y = 0, 0
+
+dx = [0, 1, 0, -1]
+dy = [-1, 0 , 1, 0]
+
+commands = list(input())
+cur_dir = 0
+
+def rotate(command):
+    global cur_dir
+    if command == 'L':
+        cur_dir = (cur_dir + 3) % 4
+    else:
+        cur_dir = (cur_dir + 1) % 4
+
+for command in commands:
+    if command == 'F':
+        x += dx[cur_dir]
+        y += dy[cur_dir]
+    else:
+        rotate(command)
+
+print(x, y)
