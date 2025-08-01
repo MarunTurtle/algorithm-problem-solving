7c2155695581744f6ea1a79be24154fc33a114d7
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-01 22:04:47 +0900

    Title: 되돌아오기 2, Time: 60ms, Memory: 20MB, Status: Wrong Answer - Codetree

---
+++ 250801/되돌아오기 2/come-back-2.py
@@ -0,0 +1,33 @@
+commands = list(input())
+n = len(commands)
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+dir_num = 0
+x, y = 0, 0
+ans = -1
+time = 0
+
+def move(command):
+    global x, y
+    global dir_num, time
+
+    if command == 'F':
+        x += dx[dir_num]
+        y += dy[dir_num]
+    elif command == 'L':
+        dir_num = (dir_num + 3) % 4
+    elif command == 'R':
+        dir_num = (dir_num + 1) % 4
+    time += 1
+    if x == 0 and y == 0:
+        return True
+    
+    return False
+
+for command in commands:
+    if move(command):
+        ans = time
+
+print(ans)
