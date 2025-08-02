c3b8ec8ad563776de5495bb08b7097a2a928e1ae
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-02 23:31:34 +0900

    Title: 모이자, Time: 52ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250802/모이자/gather.py
+++ 250802/모이자/gather.py
@@ -4,13 +4,10 @@ A = list(map(int, input().split()))
 min_sum = (n * 100) + 1
 
 for i in range(n):
-    # print(f"{i}번째")
     cur_sum = 0
-    # print(cur_sum)
     for j in range(n):
         distance = abs(i - j)
         cur_sum += A[j] * distance
-        # print(f"{cur_sum}, {i} - {j}")
     min_sum = min(min_sum, cur_sum)
 
 print(min_sum)
