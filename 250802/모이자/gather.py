56ae23bac84cc1cad92425ab4265613909f33e19
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-02 23:31:18 +0900

    Title: 모이자, Time: 49ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250802/모이자/gather.py
+++ 250802/모이자/gather.py
@@ -1,13 +1,17 @@
 n = int(input())
 A = list(map(int, input().split()))
 
-min_sum = float('inf')
+min_sum = (n * 100) + 1
 
-for i in range(n):  # i: 사람들이 모일 집의 인덱스
+for i in range(n):
+    # print(f"{i}번째")
     cur_sum = 0
+    # print(cur_sum)
     for j in range(n):
         distance = abs(i - j)
-        cur_sum += A[j] * distance  # j번 집에 있는 A[j]명 모두 이동
+        cur_sum += A[j] * distance
+        # print(f"{cur_sum}, {i} - {j}")
     min_sum = min(min_sum, cur_sum)
 
 print(min_sum)
+
