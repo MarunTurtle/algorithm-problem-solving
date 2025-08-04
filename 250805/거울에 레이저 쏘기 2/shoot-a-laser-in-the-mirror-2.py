d9d46ba643f3777d095acf77698e6d25004b7e5e
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-05 01:27:31 +0900

    Title: 거울에 레이저 쏘기 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250805/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 250805/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -7,22 +7,18 @@ count = 0
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
 
+def calc_cur_dir(k):
+    return (k-1) // n
 
 def calc_cur_place(k):
     if calc_cur_dir(k) == 0:
         return (0, k-1)
     elif calc_cur_dir(k) == 1:
-        return (k-1 % n, n-1)
+        return ((k-1) % n, n-1)
     elif calc_cur_dir(k) == 2:
         return (n-1, n - 1 - ((k-1) % n))
     else:
         return(n - 1 - ((k-1) % n) , 0)
-
-def calc_cur_dir(k):
-    return (k-1) // n
-
-x, y = calc_cur_place(k)
-cur_dir = calc_cur_dir(k)
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
@@ -46,6 +42,9 @@ def mirror_change_dir(mirror):
         return True
     else:
         return False
+
+cur_dir = calc_cur_dir(k)
+x, y = calc_cur_place(k)
 
 while True:
     mirror = grid[x][y]
