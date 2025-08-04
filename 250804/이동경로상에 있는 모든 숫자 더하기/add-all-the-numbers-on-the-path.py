c6c3690df630a93a5933ffd30109abf9a3ea4a1a
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-04 22:43:09 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250804/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -0,0 +1,36 @@
+n, t = map(int, input().split())
+commands = list(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# U, R, D, L
+dx = [0, 1, 0, -1]
+dy = [-1, 0, 1, 0]
+cur_dir = 0
+total = 0
+x, y = (n+1)//2 - 1, (n+1)//2 - 1
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def move(command):
+    global x, y
+    global grid
+    global cur_dir
+    global total
+    
+    if command == 'L':
+        cur_dir = (cur_dir + 3) % 4
+    elif command == 'R':
+        cur_dir = (cur_dir + 1) % 4
+    else:
+        nx = x + dx[cur_dir]
+        ny = y + dy[cur_dir]
+        if in_range(nx, ny):
+            x += dx[cur_dir]
+            y += dy[cur_dir]
+            total += grid[x][y]
+
+for command in commands:
+    move(command)
+
+print(total)
