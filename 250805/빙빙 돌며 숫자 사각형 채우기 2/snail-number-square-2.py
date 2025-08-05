257c96bfde6f306b1cce160c3b06ecbaf2d991f0
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-05 12:02:34 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250805/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
+++ 250805/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -8,22 +8,22 @@ dy = [0, 1, 0, -1]
 
 cur_dir = 0
 grid = [[0] * m for _ in range(n)]
+grid[x][y] = 1
 
 def in_range(r, c):
     return 0 <= r < n and 0 <= c < m and grid[r][c] == 0
 
-for i in range(1, (n * m) + 1):
-    grid[x][y] = i
+for i in range(2, (n * m) + 1):
+    while True:
+        nx = x + dx[cur_dir]
+        ny = y + dy[cur_dir]
 
-    nx = x + dx[cur_dir]
-    ny = y + dy[cur_dir]
-
-    if in_range(nx, ny):
-        x, y = nx, ny
-    else:
-        cur_dir = (cur_dir + 1) % 4
-        x += dx[cur_dir]
-        y += dy[cur_dir]
+        if in_range(nx, ny):
+            x, y = nx, ny
+            grid[x][y] = i
+            break
+        else:
+            cur_dir = (cur_dir + 1) % 4
 
 for row in grid:
     for num in row:
