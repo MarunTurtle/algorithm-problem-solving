8fbc682c561730ce8f7490e0206d2bdd021fe861
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-06 20:56:39 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250806/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,24 @@
+import sys
+MIN_DIST = sys.maxsize
+
+n = int(input())
+checkpoints = [tuple(map(int, input().split())) for _ in range(n)]
+
+min_distance = MIN_DIST
+
+x, y = checkpoints[0][0], checkpoints[0][1]
+
+for i in range(1, n-1):
+    curr_distance = 0
+    for j in range(1, n):
+        if j == i:
+            continue
+        else:
+            nx, ny = checkpoints[j][0], checkpoints[j][1]
+            curr_distance += abs(nx - x) + abs(ny - y)
+            x, y = nx, ny
+            
+    min_distance = min(min_distance, curr_distance)
+    x, y = checkpoints[0][0], checkpoints[0][1]
+
+print(min_distance)
