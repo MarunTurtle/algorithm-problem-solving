72e8018b217e6afc8103378dd84f2889497c2227
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-06 17:43:02 +0900

    Title: 체크판위에서 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250806/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+grid = [list(input().split()) for _ in range(n)]
+
+# Please write your code here.
+
+def is_jumpable(x, y, nx, ny):
+    return x < nx and y < ny and grid[nx][ny] != grid[x][y]
+
+
+cnt = 0
+x, y = 0, 0
+trgt_x, trgt_y = n-1, m-1
+
+for i in range(1, n-1):
+    for j in range(1, m-1):
+        if is_jumpable(x, y, i, j):
+            for k in range(i+1, n-1):
+                for l in range(j+1, m-1):
+                    if is_jumpable(i, j, k, l):
+                        if k < n-1 and k < m-1:
+                            cnt += 1
+
+print(cnt)
+
+
