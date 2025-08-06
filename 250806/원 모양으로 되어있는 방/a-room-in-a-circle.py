7ce188eca80b3469349e2546c772ae6c09d754c2
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-06 21:29:14 +0900

    Title: 원 모양으로 되어있는 방, Time: 73ms, Memory: 17MB, Status: Passed - Codetree

--- 250806/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 250806/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -8,11 +8,9 @@ people = sum(room_capacity)
 
 for i in range(n):      # 모든 방을 시작점으로
     cur_mov = 0     
-    cur_people = people
     for j in range(n):  # 해당 방을 시작점으로 한 바퀴 돌기
         cur_idx = (i + j) % n
         cur_mov += room_capacity[cur_idx] * j
-        cur_people -= room_capacity[cur_idx]
     
     ans = min(ans, cur_mov)
 
