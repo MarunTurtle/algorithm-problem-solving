4c96a35eb912d124d042410a3d884a0cb4141e8e
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-07 14:14:24 +0900

    Title: 최고의 13위치 2, Time: 51ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250807/최고의 13위치 2/best-place-of-13-2.py
+++ 250807/최고의 13위치 2/best-place-of-13-2.py
@@ -1,26 +1,23 @@
-# 입력 받기
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-# 사전 계산: 3칸짜리 수평선의 합을 저장할 배열
-line_sum = [[0] * (n - 2) for _ in range(n)]
+sum_list = [[0] * (n-2) for _ in range(n)]
 
-# 1. 각 행마다 가능한 3칸짜리 구간의 합을 미리 계산
 for i in range(n):
-    for j in range(n - 2):
-        line_sum[i][j] = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+    for j in range(n-2):
+        sum_list[i][j] = grid[i][j] + grid[i][j+1] + grid[i][j+2]
+    
+ans = -1
 
-# 2. 두 수평선 조합 비교 (겹치지 않도록)
-max_cnt = 0
 for i in range(n):
-    for j in range(n - 2):
+    for j in range(n-2):
         for k in range(n):
-            for l in range(n - 2):
-                # 겹치는 경우 패스
-                if i == k and abs(j - l) <= 2:
+            for l in range(n-2):
+                if i == k and abs(j - 1) <= 2:
                     continue
                 
-                cnt = line_sum[i][j] + line_sum[k][l]
-                max_cnt = max(max_cnt, cnt)
+                first = sum_list[i][j]
+                second = sum_list[k][l]
+                ans = max(ans, first+second)
 
-print(max_cnt)
+print(ans)
