ae5b4c409fef8110d7e557a3e0eb0b04ea9d64d1
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-07 09:26:38 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250807/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 250807/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -5,7 +5,7 @@ n = len(string)
 # 모든 쌍을 다 잡아봅니다.
 cnt = 0
 for i in range(n - 1):
-    for j in range(i + 2, n - 1):
+    for j in range(i + 1, n - 1):
         if string[i] == '(' and string[i + 1] == '(' and string[j] == ')' and string[j + 1] == ')':
             cnt += 1
             
