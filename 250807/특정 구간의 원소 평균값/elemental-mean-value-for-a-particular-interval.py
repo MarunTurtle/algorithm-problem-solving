a90b4ac5ba15c8464c6f8ff31801259d0ccffd44
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-07 21:16:49 +0900

    Title: 특정 구간의 원소 평균값, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 250807/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 250807/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -10,7 +10,7 @@ for i in range(n):
         length = j + 1 - i
         if total_sum % length == 0:
             avg = total_sum // length  # 정수 평균
-            if avg in arr:
+            if avg in arr[i:j+1]:
                 count += 1
 
 print(count)
