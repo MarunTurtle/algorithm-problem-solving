53879a702997a68725d897521d16fb4682a7b29a
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-07 20:02:33 +0900

    Title: 특정 수와 근접한 합, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

--- 250807/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 250807/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -6,7 +6,7 @@ nums = list(map(int, input().split()))
 min_diff = sys.maxsize 
 
 for i in range(n-1):
-    for j in range(i, n):
+    for j in range(i+1, n):
         num_sum = sum(nums) - nums[i] - nums[j]
         diff = abs(s - num_sum)
         min_diff = min(min_diff, diff)
