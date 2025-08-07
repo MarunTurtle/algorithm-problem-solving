7b18f4d63179ddb0ee2808c68424c7b7784378e9
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-07 23:50:27 +0900

    Title: G or H 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250807/G or H 2/G-or-H-2.py
+++ 250807/G or H 2/G-or-H-2.py
@@ -1,5 +1,6 @@
 n = int(input())
-arr = [0] * 101
+MAX = 100
+arr = [0] * (MAX + 1)
 
 for i in range(n):
     idx, letter = input().split()
@@ -8,21 +9,29 @@ for i in range(n):
 
 max_size = 0
 
-#G 최댓값
-for i in range(n):
+#H, G 각각 최댓값
+for i in range(MAX + 1):
     size = 0
-    if i != 1:
-        continue
-    for j in range(i, n):
-            
-
-
-
-
-for i in range(n):
     if arr[i] == 0:
-            continue
-    for j in range(i, n):
+        continue
+    for j in range(i, MAX + 1):
+        if arr[j] == arr[i]:
+            size = j - i
+        else:
+            break
+        max_size = max(size, max_size)
+
+# GH 정확히 같은 갯수
+for i in range(MAX + 1):
+    size = 0
+    if arr[i] == 0:
+        continue
+    for j in range(i, MAX + 1):
         if arr[j] == 0:
             continue
-        
+        temp_arr = arr[i:j+1]
+        if temp_arr.count(1) == temp_arr.count(2):
+            size = j - i
+            max_size = max(size, max_size)
+
+print(max_size)
