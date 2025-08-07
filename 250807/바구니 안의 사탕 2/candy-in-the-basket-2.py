0ec3751c0b05f911c003e24cae7357bd8fb701e9
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-07 21:51:20 +0900

    Title: 바구니 안의 사탕 2, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 250807/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250807/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -9,16 +9,10 @@ for _ in range(n):
 
 max_sum = 0
 
-for i in range(MAX_NUM - k + 1):
-    if i - k >= 0 and i + k <= MAX_NUM:
-        cur_sum = sum(arr[i-k:i+k+1])
-    elif i - k < 0 and i + k > MAX_NUM:
-        cur_sum = sum(arr)
-    elif i - k < 0:
-        cur_sum = sum(arr[0:i+k+1])
-    else:
-        cur_sum = sum(arr[i-k:])
+for i in range(MAX_NUM + 1):
+    left = max(0, i - k)
+    right = min(MAX_NUM, i + k)
+    cur_sum = sum(arr[left:right+1])
     max_sum = max(max_sum, cur_sum)
 
 print(max_sum)
-
