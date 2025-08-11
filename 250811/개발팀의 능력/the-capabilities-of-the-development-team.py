b33d4bb5a623592562a4e289bc15665003dfacad
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-11 21:51:57 +0900

    Title: 개발팀의 능력, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250811/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+
+arr = list(map(int, input().split()))
+
+n = 5
+
+ans = sys.maxsize
+
+def diff(i, j, k, l):
+    sum1 = arr[i] + arr[j]
+    sum2 = arr[k] + arr[l]
+    sum3 = sum(arr) - sum1 - sum2
+
+    max_sum = max(sum1, sum2, sum3)
+    min_sum = min(sum1, sum2, sum3)
+
+    return max_sum - min_sum
+
+for i in range(n):
+    for j in range(i+1, n):
+        
+        for k in range(n):
+            for l in range(k+1, n):
+                if k == i or k == j or l == i or l == j:
+                    continue
+                dif =  diff(i, j, k, l)
+                if dif != 0:
+                    ans = min(ans, dif)
+                    # print(f"{i, j, k, l} | {ans}")
+
+if ans == sys.maxsize:
+    print(-1)
+else: 
+    print(ans)
