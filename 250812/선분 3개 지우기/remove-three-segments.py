86c50daa9112d59a0332420865df6f3a6d7df170
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-12 14:39:36 +0900

    Title: 선분 3개 지우기, Time: 58ms, Memory: 17MB, Status: Passed - Codetree

--- 250812/선분 3개 지우기/remove-three-segments.py
+++ 250812/선분 3개 지우기/remove-three-segments.py
@@ -2,14 +2,22 @@ n = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(n)]
 count = 0
 
-for i in range(n-1):
-    for j in range(i+1, n):
-        x1, x2 = lines[i]
-        x3, x4 = lines[j]
-        
-        if x1 < x4 and x2 < x3:
-            count += 1
-        elif x3 < x2 and x4 < x1:
-            count += 1
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            grid = [0] * (100+1)
+            is_seperate = True
+            for l in range(n):
+                if l == i or l == j or l == k:
+                    continue
+                else:
+                    for m in range(lines[l][0], lines[l][1] + 1):
+                        if grid[m] == 1:
+                            is_seperate = False
+                        else:
+                            grid[m] += 1
+            if is_seperate:
+                count += 1
+
 
 print(count)
