f8fdff932ce903d0e7270f18fa3e0d6dfa3db2a2
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-12 11:03:11 +0900

    Title: 스승의 은혜 2, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250812/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+# 1 <= n <= 1 000
+# 1 <= b <= 1 000
+# 0 <= price <= 1 000 (ALWAYS EVEN)
+
+n, budget = map(int, input().split())
+presents = [int(input()) for _ in range(n)]
+max_count = 0
+
+for i in range(n):
+    count = 0
+    budget_cut = budget
+    for j in range(n):
+        present_price = 0
+        if j == i:
+            present_price = (presents[j]//2)
+        else:
+            present_price = presents[j]
+        
+        if budget_cut < present_price:
+            break
+        else:
+            budget_cut -= present_price
+            count += 1
+    max_count = max(count, max_count)
+
+print(max_count)
