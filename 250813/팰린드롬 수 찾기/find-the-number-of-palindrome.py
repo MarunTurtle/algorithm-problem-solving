948923636597de7903dd234cfa2c22efa1103c67
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-13 17:26:12 +0900

    Title: 팰린드롬 수 찾기, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 250813/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 250813/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -4,16 +4,7 @@ count = 0
 # Please write your code here.
 for num in range(x, y+1):
     num_to_str = str(num)
-    r = len(num_to_str)
-    if r % 2 == 0:
-        l = len(num_to_str)//2
-        left = num_to_str[:l]
-        right = num_to_str[l:][::-1]
-    else:
-        l = (r+1)//2
-        left = num_to_str[:l]
-        right = num_to_str[l-1:][::-1]
-    if left == right:
+    if num_to_str == num_to_str[::-1]:
         count += 1
 
 print(count)
