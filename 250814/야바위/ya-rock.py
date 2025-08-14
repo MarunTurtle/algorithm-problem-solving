5d8ca5e92cf66b6b15c5bd352b7c2cdceb170e05
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-14 21:44:19 +0900

    Title: 야바위, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250814/야바위/ya-rock.py
@@ -0,0 +1,26 @@
+n = int(input())
+moves = [tuple(map(int, input().split())) for _ in range(n)]
+a, b, c = zip(*moves)
+a, b, c = list(a), list(b), list(c)
+
+max_point = 0
+max_location = 0
+
+for i in range(1, 4):
+    location_rock = i   # fixed location 1, 2, 3
+    point = 0
+    for j in range(n):
+        # print(j, location_rock)
+        if a[j] == location_rock:
+            location_rock = b[j]
+        elif b[j] == location_rock:
+            location_rock = a[j]
+        
+        if location_rock == c[j]:
+            point += 1
+
+    if point > max_point:
+        max_point = point
+        max_location = i
+    
+print(max_location)
