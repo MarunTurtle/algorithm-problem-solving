0d20e1d5bcb589cdb00900b182503cec1b1a8be0
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-26 15:19:25 +0900

    Title: 초기 수열 복원하기, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 250826/초기 수열 복원하기/restore-initial-sequence.py
+++ 250826/초기 수열 복원하기/restore-initial-sequence.py
@@ -5,7 +5,7 @@ for i in range(1, nums[0]): # 첫번재 숫자 후보들
     new_nums = [i]
     for j in range(n-1):
         next_num = nums[j] - new_nums[j]
-        if next_num <= 0 or next_num == new_nums[j]:
+        if next_num <= 0 or next_num in new_nums:
             break
         else:
             new_nums.append(next_num)
