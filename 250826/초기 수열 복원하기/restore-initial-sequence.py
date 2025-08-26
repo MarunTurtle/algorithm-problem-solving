77b5a9d392c7e22721f9eee9927cd75c8ad8ebd0
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-26 15:18:31 +0900

    Title: 초기 수열 복원하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250826/초기 수열 복원하기/restore-initial-sequence.py
@@ -0,0 +1,13 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+for i in range(1, nums[0]): # 첫번재 숫자 후보들
+    new_nums = [i]
+    for j in range(n-1):
+        next_num = nums[j] - new_nums[j]
+        if next_num <= 0 or next_num == new_nums[j]:
+            break
+        else:
+            new_nums.append(next_num)
+    if len(new_nums) == n:
+        print(*new_nums)
