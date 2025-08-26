ee8ebe23e732797c3c795099c1dc762c2e6cc8a0
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-26 11:17:04 +0900

    Title: 훌륭한 점프, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250826/훌륭한 점프/great-jump.py
+++ 250826/훌륭한 점프/great-jump.py
@@ -4,13 +4,13 @@ n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 def is_possible(a):
-    picked = [-k] + [i for i, x in enumerate(arr) if x <= a] + [n - 1]
+    picked = [0] + [i for i, x in enumerate(arr) if x <= a] + [n - 1]
     for i in range(1, len(picked)):
         if picked[i] - picked[i - 1] > k:
             return False
     return True
 
-for a in range(1, max(arr) + 1):
+for a in range(arr[0], max(arr) + 1):
     if is_possible(a):
         print(a)
         break
