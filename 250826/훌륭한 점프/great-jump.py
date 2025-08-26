18a94fcb6f5690d1b550230285fefbd298e299d1
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-26 12:02:37 +0900

    Title: 훌륭한 점프, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

--- 250826/훌륭한 점프/great-jump.py
+++ 250826/훌륭한 점프/great-jump.py
@@ -1,16 +1,25 @@
 import sys
+input = sys.stdin.readline
 
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 def is_possible(a):
-    picked = [i for i, x in enumerate(arr) if x <= a]
+
+    if arr[0] > a or arr[-1] > a:
+        return False
+
+    picked = [i for i, v in enumerate(arr) if v <= a]
+    
+    if picked[0] != 0 or picked[-1] != n-1:
+        return False
+
     for i in range(1, len(picked)):
         if picked[i] - picked[i - 1] > k:
             return False
     return True
 
-for a in range(arr[0], max(arr)+1):
+for a in range(max(arr[-1], arr[0]), max(arr)+1):
     if is_possible(a):
         print(a)
         break
