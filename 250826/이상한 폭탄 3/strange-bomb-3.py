f681b56dc02e274ca827284fcf5cd38619320f66
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-26 16:26:43 +0900

    Title: 이상한 폭탄 3, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250826/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,25 @@
+n, k = tuple(map(int, input().split()))
+bombs = [int(input()) for _ in range(n)]
+
+bomb_num = 0
+bomb_cnt = 0
+
+def explode(x, bomb_exploded):
+    if x == n-1:
+        return bomb_exploded
+    bomb_num = bombs[x]
+    for i in range(x+1, n):
+        if bomb_exploded[i] == 0 and bombs[i] == bomb_num and i - x <= k:
+            bomb_exploded[i] = 1
+            bomb_exploded = explode(i, bomb_exploded)
+    return bomb_exploded
+
+for i in range(n):
+    bomb_exploded = [0] * n
+    bomb_exploded = explode(i, bomb_exploded)
+    cur_bomb_cnt = bomb_exploded.count(1)
+    if cur_bomb_cnt > bomb_cnt:
+        bomb_num = bombs[i]
+        bomb_cnt = cur_bomb_cnt
+
+print(bomb_num)
