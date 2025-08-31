0271c56291ae61e234c50f8c93906fcb92958250
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-31 20:22:38 +0900

    Title: X 달리기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250831/X 달리기/x-run.py
@@ -0,0 +1,19 @@
+target_distance = int(input())
+
+def total_distance_with(n):
+    return ((n * (n+1)) // 2) + (((n - 1) * (n)) // 2)
+
+top_speed = 1
+remaining_distance = 0
+ans = float('inf')
+
+while True:
+    if total_distance_with(top_speed) > target_distance:
+        break
+    remaining_distance = target_distance - total_distance_with(top_speed)
+    ans = min(ans, top_speed * 2 - 1 + remaining_distance)
+    # print(f"top_speed {top_speed} total_distance {total_distance_with(top_speed)}, remaining_distance {remaining_distance} cur_ans {top_speed * 2 - 1 + remaining_distance}")
+    top_speed += 1
+
+print(ans)
+
