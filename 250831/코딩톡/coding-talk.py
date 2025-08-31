6c907ee1c0b7fa2c015f5c4304790c4079dead9c
Jaehyun Kevin Lee <angle0918@naver.com>
2025-08-31 16:52:43 +0900

    Title: 코딩톡, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250831/코딩톡/coding-talk.py
@@ -0,0 +1,13 @@
+# n 사람 수 / m 메시지 수 / p 확인 번호
+# m개의 메시지 - c 메시지 올린 사람, u 아직 읽지 않은 사람
+n, m, p = map(int, input().split())
+arr = [0] + [tuple(input().split()) for _ in range(m)]
+devs = [0] + [arr[i][0] for i in range(1, m+1)]
+unreads = [0] + [int(arr[i][1]) for i in range(1, m+1)]
+dev_list = [chr(ord('A') + i) for i in range(n)]
+
+for i in range(p, m+1):
+    if devs[i] in dev_list:
+        dev_list.remove(devs[i])
+
+print(*dev_list)
