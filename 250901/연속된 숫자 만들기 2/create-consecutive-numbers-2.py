9209074a8046c3b4323287a017241179f4ff850b
Jaehyun Kevin Lee <angle0918@naver.com>
2025-09-01 22:31:41 +0900

    Title: 연속된 숫자 만들기 2, Time: 56ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250901/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -0,0 +1,13 @@
+n = 3
+pos = list(map(int, input().split()))
+
+diff12 = pos[1] - pos[0]
+diff23 = pos[2] = pos[1]
+
+if diff12 == 1 and diff23 == 1:
+    print(0)
+elif diff12 == 1 or diff23 == 1:
+    print(1)
+else:
+    print(2)
+
