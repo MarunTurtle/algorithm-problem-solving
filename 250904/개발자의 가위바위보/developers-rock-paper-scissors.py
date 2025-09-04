2e1149551b5f8f045788fa0d4264c22ea65bac91
Jaehyun Kevin Lee <angle0918@naver.com>
2025-09-04 16:31:35 +0900

    Title: 개발자의 가위바위보, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/개발자의 가위바위보/developers-rock-paper-scissors.py
+++ 250904/개발자의 가위바위보/developers-rock-paper-scissors.py
@@ -9,16 +9,18 @@ max_ans = 0
 scenarios = list(itertools.permutations([1, 2, 3], 3))
 
 for scenario in scenarios:
-    print(f"scenario: {scenario}")
+    # print(f"scenario: {scenario}")
     ans = 0
     for i in range(n):
         a = me[i]
         b = you[i]
         power_a = scenario.index(a)
         power_b = scenario.index(b)
-        print(power_a, power_b)
+        # print(power_a, power_b)
 
-        if power_a == 0 and power_b == 2:
+        if power_a == 2 and power_b == 0:
+            continue
+        elif power_a == 0 and power_b == 2:
             ans += 1
         elif power_a > power_b:
             ans += 1
