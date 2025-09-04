501fca50be19b3ca10b22701d1f20b7762de6edb
Jaehyun Kevin Lee <angle0918@naver.com>
2025-09-04 15:37:37 +0900

    Title: 독서실의 거리두기 3, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 250904/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -2,19 +2,22 @@ n = int(input())
 seats = list(map(int, list(input())))
 count = []
 
-cur_count = 0
+cur_count = 1
 for i in range(1, n):
     if seats[i] == 0:
         cur_count += 1
     else:
         count.append(cur_count)
-        cur_count = 0
-
-count.sort()
+        cur_count = 1
+# print(count)
+count.sort(reverse=True)
+# print(count)
 ans = 0
-for i in range(len(count)):
-    count[i] -= 1
-    ans = max(ans, min(count))
-    count[i] += 1
-
-print(ans)
+new_a = count[0] // 2
+new_b = count[0] - new_a
+del count[0]
+# print(count)
+count.append(new_a)
+count.append(new_b)
+# print(count)
+print(min(count))
