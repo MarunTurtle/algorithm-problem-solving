0a967a4efad09c799706457869f497c02e664e3f
Jaehyun Kevin Lee <angle0918@naver.com>
2025-09-04 22:15:20 +0900

    Title: 독서실의 거리두기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250904/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,33 @@
+n = int(input())
+seats = list(map(int, list(input())))
+ans = 0 
+
+def count_min_dist(seats):
+    ans = 0
+    diffs = []
+    count = 0
+    can_start = False 
+    for i in range(n):
+        if i == 0:
+            if seats[i] == 0:
+                can_start = False
+                continue
+            else:
+                can_start = True
+        elif seats[i] == 1:
+            count += 1
+            can_start = True
+            diffs.append(count)
+            count = 0
+        elif can_start:
+            if seats[i] == 0:
+                count += 1
+    return min(diffs)
+
+for i in range(n):
+    if seats[i] == 0:
+        seats[i] = 1
+        ans = max(ans, count_min_dist(seats))
+        seats[i] = 0
+
+print(ans)
