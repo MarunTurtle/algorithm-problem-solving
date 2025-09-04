f8dfb53cfb2a4ba0346bd900871b95271933f47a
Jaehyun Kevin Lee <angle0918@naver.com>
2025-09-04 17:27:22 +0900

    Title: 개발자의 가위바위보, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/개발자의 가위바위보/developers-rock-paper-scissors.py
+++ 250904/개발자의 가위바위보/developers-rock-paper-scissors.py
@@ -1,29 +1,34 @@
-import itertools
-
+# 변수 선언 및 입력
 n = int(input())
-matches = [tuple(map(int, input().split())) for _ in range(n)]
-me, you = zip(*matches)
-me, you = list(me), list(you)
-max_ans = 0
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-scenarios = list(itertools.permutations([1, 2, 3], 3))
+max_win = 0
 
-for scenario in scenarios:
-    # print(f"scenario: {scenario}")
-    ans = 0
-    for i in range(n):
-        a = me[i]
-        b = you[i]
-        power_a = scenario.index(a)
-        power_b = scenario.index(b)
-        # print(power_a, power_b)
+# Case 1. 1이 2를 이기고, 2가 3을 이기고 3이 1을 이기는 경우
+win = 0
+for a, b in arr:
+    if a == 1 and b == 2:
+        win += 1
+    elif a == 2 and b == 3:
+        win += 1
+    elif a == 3 and b == 1:
+        win += 1
 
-        if power_a == 2 and power_b == 0:
-            continue
-        elif power_a == 0 and power_b == 2:
-            ans += 1
-        elif power_a > power_b:
-            ans += 1
-    max_ans = max(max_ans, ans)
+max_win = max(max_win, win)
 
-print(max_ans)
+# Case 2. 1이 3을 이기고, 3이 2를 이기고 2가 1을 이기는 경우
+win = 0
+for a, b in arr:
+    if a == 1 and b == 3:
+        win += 1
+    elif a == 3 and b == 2:
+        win += 1
+    elif a == 2 and b == 1:
+        win += 1
+
+max_win = max(max_win, win)
+
+print(max_win)
