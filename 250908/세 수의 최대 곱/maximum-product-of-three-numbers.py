03e278e92159037a71f20e9089907c19a0053047
Jaehyun Kevin Lee <angle0918@naver.com>
2025-09-08 23:20:27 +0900

    Title: 세 수의 최대 곱, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250908/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 250908/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,14 +1,15 @@
 from itertools import combinations
 
 n = int(input())
-arr = list(map(int, input().split()))
+sort_arr = list(map(int, input().split()))
+sort_arr.sort(reverse=True)
 int_arr = [0] * 3  # 음수, 0, 양수
 ans = float('-inf')
 
 for i in range(n):
-    if arr[i] > 0:
+    if sort_arr[i] > 0:
         int_arr[2] += 1
-    elif arr[i] < 0:
+    elif sort_arr[i] < 0:
         int_arr[0] += 1
     else:
         int_arr[1] += 1
@@ -18,17 +19,16 @@ if int_arr[0] == 0 and int_arr[2] == 0:
 elif int_arr[2] == 0 and int_arr[1] >= 1:
     ans = 0
 elif int_arr[2] == 0 and int_arr[1] == 0:
-    sort_arr = arr.sort(reverse=True)
     ans = sort_arr[0] * sort_arr[1] * sort_arr[2]
 elif int_arr[0] == 0:
-    sort_arr = arr.sort(reverse=True)
     ans = sort_arr[0] * sort_arr[1] * sort_arr[2]
-else:
-    for i in range(n-2):
-        for j in range(i+1, n-1):
-            for k in range(j+1, n):
-                # if ans < arr[i] * arr[j] * arr[k]:
-                #     print(arr[i], arr[j], arr[k])
-                ans = max(ans, arr[i] * arr[j] * arr[k])
+elif int_arr[2] == 1:
+    ans = arr[0] * arr[n-1] * arr[n-2]
+elif int_arr[0] == 1:
+    ans = arr[0] * arr[1] * arr[2]
+else: 
+    ans_sub_1 = sort_arr[0] * sort_arr[1] * sort_arr[2]
+    ans_sub_2 = sort_arr[n-1] * sort_arr[n-2] * sort_arr[0]
+    ans = max(ans_sub_2, ans_sub_1)
     
 print(ans)
