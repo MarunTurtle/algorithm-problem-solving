e66ba6ddedbd77b025a7e08af022b31269d6d1ed
Jaehyun Kevin Lee <angle0918@naver.com>
2025-09-08 17:54:40 +0900

    Title: 순위 경쟁, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250908/순위 경쟁/ranking-competition.py
@@ -0,0 +1,35 @@
+n = int(input())
+c, s = [], []
+
+hall_of_fame = set()
+count = 0
+
+cur_scores = [0, 0, 0]
+
+for i in range(n):
+    player, score = input().split()
+    score = int(score)
+
+    if player == 'A':
+        cur_scores[0] += score
+    elif player == 'B':
+        cur_scores[1] += score
+    else:
+        cur_scores[2] += score
+    
+    max_point = max(cur_scores)
+    cur_winner = []
+
+    for i in range(3):
+        if cur_scores[i] == max_point:
+            cur_winner += chr(97 + i)
+    
+    if set(cur_winner) != hall_of_fame:
+        count += 1
+        hall_of_fame = set(cur_winner)
+
+print(count)
+    
+    
+
+
