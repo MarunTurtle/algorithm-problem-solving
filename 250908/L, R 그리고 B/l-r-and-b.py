ebf97d053525b56b72dc0adbcd2113131894e5dd
Jaehyun Kevin Lee <angle0918@naver.com>
2025-09-08 15:33:50 +0900

    Title: L, R 그리고 B, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250908/L, R 그리고 B/l-r-and-b.py
+++ 250908/L, R 그리고 B/l-r-and-b.py
@@ -3,10 +3,10 @@ board = [list(input()) for _ in range(10)]
 for i in range(10):
     for j in range(10):
         if board[i][j] == 'B':
-            b = tuple(i, j)
+            b = [i, j]
         elif board[i][j] == 'R':
-            r = tupel(i, j)
+            r = [i, j]
         elif board[i][j] == 'L':
-            l = tuple(i, j)
+            l = [i, j]
 
-print(1 + b[0] - l[0] + b[1] - l[1])
+print(abs(1 + b[0] - l[0] + b[1] - l[1]))
