1810e51414c126342167ff57bd503c1adacbd759
Jaehyun Kevin Lee <angle0918@naver.com>
2025-09-09 10:20:03 +0900

    Title: 최소 와이파이 수, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250909/최소 와이파이 수/minimum-number-of-wifi.py
+++ 250909/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,18 +1,13 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-arr = [0] + arr
-count = 0
-covered_end = 0
+cnt, i = 0, 0
 
-i = 1
-while i <= n:
-    if arr[i] == 1 and i > covered_end:
-        p = min(n, i + m)
-        count += 1
-        covered_end = p + m
-        i = covered_end + 1
+while i < n:
+    if arr[i] == 1:
+        cnt += 1
+        i += 2 * m + 1
     else:
         i += 1
 
-print(count)
+print(cnt)
