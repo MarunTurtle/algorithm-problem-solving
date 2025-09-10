ad58bf29359d3ac588e9e25c61c4c5b9c620811a
Jaehyun Kevin Lee <angle0918@naver.com>
2025-09-10 21:51:06 +0900

    Title: 전부 포함하는 선분, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250910/전부 포함하는 선분/all-inclusive-segment.py
+++ 250910/전부 포함하는 선분/all-inclusive-segment.py
@@ -1,17 +1,37 @@
 n = int(input())
 segments = [tuple(map(int, input().split())) for _ in range(n)]
 
-starts, ends = zip(*segments)
-starts, ends = list(starts), list(ends)
+segments.sort(key = lambda x:x[0])
+option1 = list(segments)
+# print(option1)
+del option1[0]
+# print(option1)
 
-starts.sort()
-ends.sort()
+s, e = zip(*option1)
+s, e = list(s), list(e)
+s.sort()
+e.sort()
+# print(s)
+# print(e)
 
-print(starts)
-print(ends)
+ans1 = abs(min(s) - max(e))
+# print(ans1)
 
-first = ends[-2] - starts[0]
-second = ends[-1] - starts[1]
+print("-----")
+segments.sort(key = lambda x:x[1])
+option2 = list(segments)
+# print(option2)
+del option2[-1]
+# print(option2)
 
-print(first, second)
-print(min(first, second))
+s, e = zip(*option2)
+s, e = list(s), list(e)
+s.sort()
+e.sort()
+# print(s)
+# print(e)
+
+ans2 = abs(min(s) - max(e))
+# print(ans2)
+
+print(min(ans1, ans2))
