91f6e5b57277bdb697b6c38d134979334b8805e8
Jaehyun Kevin Lee <angle0918@naver.com>
2025-09-16 10:06:07 +0900

    [Gold III] Title: 가희와 탑, Time: 60 ms, Memory: 42948 KB -BaekjoonHub

--- Python/백준/Gold/24337. 가희와 탑/가희와 탑.py
+++ Python/백준/Gold/24337. 가희와 탑/가희와 탑.py
@@ -1,10 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 n, a, b = map(int, input().split())
+
 result = [i for i in range(1, a)]
 result.append(max(a, b))
 result += [i for i in range(b-1, 0, -1)]
+
 if len(result) > n:
-    result = [-1]
+    sys.stdout.write('-1')
+    sys.exit(0)
 else:
     result = result[0:1] + [1] * (n - len(result)) + result[1:]
-import sys
+
 sys.stdout.write(' '.join([str(r) for r in result]))
