78df8c97dfd391cb8c6b0bb6f2282f2414f205e4
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-13 17:21:54 +0900

    Title: 트로미노, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251013/트로미노/tromino.py
+++ 251013/트로미노/tromino.py
@@ -3,20 +3,20 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 ans = 0
 def get_sum_1(r, c):
     max_total = 0
-    if r + 2 <= n:
-        total = 0
+    total = 0
+    if r + 2 <= n-1:
         for i in range(3):
             total += grid[r+i][c]  
         max_total = max(max_total, total)
-    if c + 2 <= m:
+    if c + 2 <= m-1:
         for i in range(3):
-            total += grid[r][c+i]  
+            total += grid[r][c+i]
         max_total = max(max_total, total)
     return max_total
 
 def get_sum_2(r, c):
     max_total = 0
-    if r + 1 <= n and c + 1 <= m:
+    if r + 1 <= n-1 and c + 1 <= m-1:
         four_sum = 0
         min_elem = float('inf')
         for i in range(2):
