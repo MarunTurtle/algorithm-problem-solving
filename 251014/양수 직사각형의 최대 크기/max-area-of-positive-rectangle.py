2bd03a0f867f6df962ef51bf523769fcc3e2cc10
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-14 11:07:06 +0900

    Title: 양수 직사각형의 최대 크기, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251014/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 251014/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,23 +1,33 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-ans = -1
+mask = [[1 if grid[i][j] > 0 else 0 for j in range(m)] for i in range(n)]
 
-for r1 in range(n):
-    for c1 in range(m):
-       for r2 in range(r1, n):
-            for c2 in range(c1, m):
-                all_pos = True
-                for i in range(r1, r2+1):
-                    for j in range(c1, c2+1):
-                        if grid[i][j] <= 0:
-                            all_pos = False
-                            break
-                    if not all_pos:
-                        break
-                
-                if all_pos:
-                    v = (abs(r1 - r2) + 1) * (abs(c1 - c2) + 1)
-                    ans = max(ans, v)
+prefix = [[0] * (m + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        prefix[i][j] = (
+            mask[i-1][j-1]
+            + prefix[i-1][j]
+            + prefix[i][j-1]
+            - prefix[i-1][j-1]
+        )
 
-print(ans)
+max_area = 0
+for r1 in range(1, n + 1):
+    for r2 in range(r1, n + 1):
+        for c1 in range(1, m + 1):
+            for c2 in range(c1, m + 1):
+                total = (
+                    prefix[r2][c2]
+                    - prefix[r1-1][c2]
+                    - prefix[r2][c1-1]
+                    + prefix[r1-1][c1-1]
+                )
+                area = (r2 - r1 + 1) * (c2 - c1 + 1)
+
+                # 모든 원소가 양수인 경우
+                if total == area:
+                    max_area = max(max_area, area)
+
+print(max_area)
