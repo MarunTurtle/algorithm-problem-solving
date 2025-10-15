1ee23aebf21564efe845f90590ecf7a47f8e23e0
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-15 13:52:57 +0900

    [level 1] Title: 최소직사각형, Time: 3.57 ms, Memory: 9.72 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,12 @@
+def solution(sizes):
+    for size in sizes:
+        size.sort()
+    
+    max_w = 0
+    max_h = 0
+    
+    for size in sizes:
+        max_w = max(max_w, size[0])
+        max_h = max(max_h, size[1]) 
+    
+    return max_w * max_h
