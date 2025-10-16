05f6bb9e195e8efdbb5c39f8cdd427635bde93a8
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-16 15:32:51 +0900

    Title: 단 한 번의 2048 시도, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251016/단 한 번의 2048 시도/one-trial-of-2048-game.py
+++ 251016/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -30,101 +30,126 @@ def move_right():
 
         grid[r] = res_temp
 
-def move_left():
-    for r in range(n):
-        tmp = [0] * n
-        cur_idx = 0
-        for i in range(n):
-            num = grid[r][i]
-            if num != 0:
-                tmp[cur_idx] = num
-                cur_idx += 1
+# def move_left():
+#     for r in range(n):
+#         tmp = [0] * n
+#         cur_idx = 0
+#         for i in range(n):
+#             num = grid[r][i]
+#             if num != 0:
+#                 tmp[cur_idx] = num
+#                 cur_idx += 1
 
-        res_temp = [0] * n
-        cur_idx = 0
-        skip = []
-        for i in range(n):
-            if i in skip:
-                continue
-            if i == n-1:
-                res_temp[cur_idx] = tmp[i]
-            elif tmp[i] == tmp[i+1]:
-                res_temp[cur_idx] = tmp[i] * 2
-                cur_idx += 1
-                skip.append(i+1)
-            else:
-                res_temp[cur_idx] = tmp[i]
-                cur_idx += 1
+#         res_temp = [0] * n
+#         cur_idx = 0
+#         skip = []
+#         for i in range(n):
+#             if i in skip:
+#                 continue
+#             if i == n-1:
+#                 res_temp[cur_idx] = tmp[i]
+#             elif tmp[i] == tmp[i+1]:
+#                 res_temp[cur_idx] = tmp[i] * 2
+#                 cur_idx += 1
+#                 skip.append(i+1)
+#             else:
+#                 res_temp[cur_idx] = tmp[i]
+#                 cur_idx += 1
 
-        grid[r] = res_temp
+#         grid[r] = res_temp
 
-def move_down():
-    for c in range(n):
-        tmp = [0] * n
-        cur_idx = n-1
-        for i in range(n-1, -1, -1):
-            num = grid[i][c]
-            if num != 0:
-                tmp[cur_idx] = num
-                cur_idx -= 1
+# def move_down():
+#     for c in range(n):
+#         tmp = [0] * n
+#         cur_idx = n-1
+#         for i in range(n-1, -1, -1):
+#             num = grid[i][c]
+#             if num != 0:
+#                 tmp[cur_idx] = num
+#                 cur_idx -= 1
 
-        res_temp = [0] * n
-        cur_idx = n-1
-        skip = []
-        for i in range(n-1, -1, -1):
-            if i in skip:
-                continue
-            if i == 0:
-                res_temp[cur_idx] = tmp[i]
-            elif tmp[i] == tmp[i-1]:
-                res_temp[cur_idx] = tmp[i] * 2
-                cur_idx -= 1
-                skip.append(i-1)
-            else:
-                res_temp[cur_idx] = tmp[i]
-                cur_idx -= 1
+#         res_temp = [0] * n
+#         cur_idx = n-1
+#         skip = []
+#         for i in range(n-1, -1, -1):
+#             if i in skip:
+#                 continue
+#             if i == 0:
+#                 res_temp[cur_idx] = tmp[i]
+#             elif tmp[i] == tmp[i-1]:
+#                 res_temp[cur_idx] = tmp[i] * 2
+#                 cur_idx -= 1
+#                 skip.append(i-1)
+#             else:
+#                 res_temp[cur_idx] = tmp[i]
+#                 cur_idx -= 1
         
-        for i in range(n):
-            grid[i][c] = res_temp[i] 
+#         for i in range(n):
+#             grid[i][c] = res_temp[i] 
 
-def move_up():
-    for c in range(n):
-        tmp = [0] * n
-        cur_idx = 0
-        for i in range(n):
-            num = grid[i][c]
-            if num != 0:
-                tmp[cur_idx] = num
-                cur_idx += 1
+# def move_up():
+#     for c in range(n):
+#         tmp = [0] * n
+#         cur_idx = 0
+#         for i in range(n):
+#             num = grid[i][c]
+#             if num != 0:
+#                 tmp[cur_idx] = num
+#                 cur_idx += 1
         
-        res_temp = [0] * n
-        cur_idx = 0
-        skip = []
-        for i in range(n):
-            if i in skip:
-                continue
-            if i == n-1:
-                res_temp[cur_idx] = tmp[i]
-            elif tmp[i] == tmp[i+1]:
-                res_temp[cur_idx] = tmp[i]* 2
-                cur_idx += 1
-                skip.append(i+1)
-            else:
-                res_temp[cur_idx] = tmp[i]
-                cur_idx += 1
+#         res_temp = [0] * n
+#         cur_idx = 0
+#         skip = []
+#         for i in range(n):
+#             if i in skip:
+#                 continue
+#             if i == n-1:
+#                 res_temp[cur_idx] = tmp[i]
+#             elif tmp[i] == tmp[i+1]:
+#                 res_temp[cur_idx] = tmp[i]* 2
+#                 cur_idx += 1
+#                 skip.append(i+1)
+#             else:
+#                 res_temp[cur_idx] = tmp[i]
+#                 cur_idx += 1
         
-        for i in range(n):
-            grid[i][c] = res_temp[i] 
+#         for i in range(n):
+#             grid[i][c] = res_temp[i] 
+
+def rotate():
+    new_grid = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            new_grid[i][j] = grid[n-j-1][i]
+
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = new_grid[i][j]
+
+    # for row in grid:
+    #     print(*row)
 
 if dir == 'R':
     move_right()
 elif dir == 'L':
-    move_left()
+    rotate()
+    rotate()
+    move_right()
+    rotate()
+    rotate()
 elif dir == 'U':
-    move_up()
+    rotate()
+    move_right()
+    rotate()
+    rotate()
+    rotate()    
 else:
-    move_down()
+    rotate()
+    rotate()
+    rotate()
+    move_right()
+    rotate()
 
 for row in grid:
     print(*row)
-        
