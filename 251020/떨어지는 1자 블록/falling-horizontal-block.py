663275148a9636b649c74993850feca6283383a4
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-20 09:32:52 +0900

    Title: 떨어지는 1자 블록, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251020/떨어지는 1자 블록/falling-horizontal-block.py
+++ 251020/떨어지는 1자 블록/falling-horizontal-block.py
@@ -4,7 +4,7 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 def get_final_row():
     for r in range(1, n):
-        for c in range(k-1, k-2+m):
+        for c in range(k-1, k-1+m):
             if grid[r][c] == 1:
                 return r - 1
     return n-1
