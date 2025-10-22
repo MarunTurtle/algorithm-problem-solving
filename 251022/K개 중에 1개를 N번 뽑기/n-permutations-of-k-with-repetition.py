471d38dd04ce519f45f86902dfcd9bcaa8aa003a
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-22 13:08:52 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 211ms, Memory: 26MB, Status: Passed - Codetree

--- 251022/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
+++ 251022/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
@@ -1,16 +1,14 @@
 k, n = map(int, input().split())
-
 ans = []
 
-def choose(curr_num):
-    if curr_num == n + 1:
+def get_permutation(curr_num):
+    if curr_num == n:
         print(*ans)
         return
     
     for i in range(1, k+1):
         ans.append(i)
-        choose(curr_num+1)
+        get_permutation(curr_num+1)
         ans.pop()
-    return 
 
-choose(1)
+get_permutation(0)
