7bccda6012f8be610f82ea3fecfcadf3350a1880
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-23 11:22:31 +0900

    Title: 알파벳과 사칙연산, Time: 92ms, Memory: 20MB, Status: Passed - Codetree

--- 251023/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 251023/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -1,36 +1,34 @@
-import sys
-
-INT_MIN = -sys.maxsize
-
 n = 6
 expression = input()
 num = [0 for _ in range(n)]
-ans = INT_MIN
+ans = float('-inf')
 
 def conv(idx):
     return num[ord(expression[idx]) - ord('a')]
 
 def calc():
-    length = len(expression)
-    value = conv(0)
-    for i in range(2, length, 2):
-        if expression[i - 1] == '+':
-            value += conv(i)
-        elif expression[i - 1] == '-':
-            value -= conv(i)
+    l = len(expression)
+    total = conv(0)
+
+    for i in range(2, l, 2):
+        if expression[i-1] == "-":
+            total -= conv(i)
+        elif expression[i-1] == '*':
+            total *= conv(i)
         else:
-            value *= conv(i)
-    return value
+            total += conv(i)
+    
+    return total
 
 def find_max(cnt):
     global ans
-    
+
     if cnt == n:
         ans = max(ans, calc())
         return
     
-    for i in range(1, 5):
-        num[cnt] = i
+    for v in range(1, 5):
+        num[cnt] = v
         find_max(cnt + 1)
 
 find_max(0)
