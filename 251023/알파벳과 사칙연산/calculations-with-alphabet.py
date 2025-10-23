aa2c8b4a3721053577e7369fc0892dfb88ce74ce
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-23 10:11:11 +0900

    Title: 알파벳과 사칙연산, Time: 73ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251023/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,39 @@
+expression = list(input())
+operators = []
+alphabets = []
+for ch in expression:
+    if ch in "-*+":
+        operators.append(ch)
+    else :
+        alphabets.append(ch)
+
+n = len(alphabets)
+ans = 0
+path = []
+
+def calculate(numbers):
+    total_ans = numbers[0]
+    for i in range(1, len(numbers)):
+        if operators[i-1] == '-':
+            total_ans = total_ans - numbers[i]
+        elif operators[i-1] == '*':
+            total_ans = total_ans * numbers[i]
+        else:
+            total_ans = total_ans + numbers[i]
+
+    return total_ans
+
+def backtrack(depth):
+    global ans
+
+    if depth == n:
+        ans = max(ans, calculate(path))
+        return
+
+    for i in range(1, 5):
+        path.append(i)
+        backtrack(depth + 1)
+        path.pop()
+
+backtrack(0)
+print(ans)
