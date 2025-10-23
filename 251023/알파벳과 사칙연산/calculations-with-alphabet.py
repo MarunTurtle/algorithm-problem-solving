5e5bbc41910aea8a45483116e12ea21c4be2536f
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-23 11:00:12 +0900

    Title: 알파벳과 사칙연산, Time: 80ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 251023/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 251023/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -1,41 +1,33 @@
-expression = list(input())
-operators = []
-alphabets = []
-for ch in expression:
-    if ch in "-*+":
-        operators.append(ch)
-    else :
-        alphabets.append(ch)
-
-variants = sorted(set(alphabets))
-n = len(variants)
+expression = input()
+n = 6
+path = [0 for _ in range(n)]
 ans = float('-inf')
-assign = {}
 
-def calculate(assign_map):
-    numbers = [assign_map[ch] for ch in alphabets]
+def conv(alpha):
+    return path[ord(expression[i]) - ord('a')] 
 
-    total_ans = numbers[0] 
-    for i in range(1, len(numbers)):
-        if operators[i-1] == '-':
-            total_ans -= numbers[i]
-        elif operators[i-1] == '*':
-            total_ans *= numbers[i]
+def calculate():
+    length = len(expression)
+    total = conv(0)
+    for i in range(2, length, 2):
+        if expression[i-1] == '+':
+            total += conv(i)
+        elif expression[i-1] == '-':
+            total -= conv(i)
         else:
-            total_ans += numbers[i]
-    return total_ans
+            total += conv(i)
+    return total
 
 def backtrack(depth):
     global ans
 
     if depth == n:
-        ans = max(ans, calculate(assign))
+        ans = max(ans, calculate())
         return
     
     for i in range(1, 5):
-        assign[variants[depth]] = i
+        path[depth] = i
         backtrack(depth + 1)
-        assign.pop(variants[depth])
 
 backtrack(0)
 print(ans)
