020d60d9cab864625a17911c3659850177daec23
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-27 15:10:23 +0900

    Title: 외판원 순회, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251027/외판원 순회/traveling-salesman-problem.py
+++ 251027/외판원 순회/traveling-salesman-problem.py
@@ -1,34 +1,28 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
+# Please write your code here.
 ans = float('inf')
-selected = []
-visited = [False] * (n+1)
+picked = []
+visited = [0] * (n)
 
-def get_sum():
-    return sum(selected)
-
-def no_zero():
-    for num in selected:
-        if num == 0:
-            return False
-    return True
-
-def get_min_sum_combo(depth):
+def get_min_path(depth):
     global ans
-    if depth == n:
-        if no_zero():
-            ans = min(ans, get_sum())
-            return
-        return
-    
-    for i in range(n):
-        if depth != i and not visited[i]:           
-            selected.append(grid[depth][i])
-            visited[i] = True
-            get_min_sum_combo(depth + 1)
-            visited[i] = False
-            selected.pop()
 
-get_min_sum_combo(0)
+    if depth == n: 
+        ans = min(ans, sum(picked))
+        print(picked)
+        return
+
+    for row in range(n):
+        if grid[row][depth] == 0:
+            continue
+        if not visited[row]:
+            picked.append(grid[row][depth])
+            visited[row] = True
+            get_min_path(depth + 1)
+            visited[row] = False
+            picked.pop()
+
+get_min_path(0)
 print(ans)
