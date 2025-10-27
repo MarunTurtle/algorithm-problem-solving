3d7b8b565182932153ef3eb98d39ebeaac34b159
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-27 15:34:24 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 198ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 251027/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,27 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+ans = 0
+selected = []
+visited = [False] * (n+1)
+
+def get_sum():
+    return sum(selected)
+
+def get_max_sum_combo(depth):
+    global ans
+    if depth == n:
+        min_value = min(selected)
+        ans = max(ans, min_value)
+        return
+    
+    for i in range(n):
+        if not visited[i]:
+            selected.append(grid[depth][i])
+            visited[i] = True
+            get_max_sum_combo(depth + 1)
+            visited[i] = False
+            selected.pop()
+
+get_max_sum_combo(0)
+print(ans)
