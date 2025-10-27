e019eab6b4817f1ad34e40516db38d70c88b8901
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-27 14:52:12 +0900

    Title: 외판원 순회, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251027/외판원 순회/traveling-salesman-problem.py
+++ 251027/외판원 순회/traveling-salesman-problem.py
@@ -8,18 +8,22 @@ visited = [False] * (n+1)
 def get_sum():
     return sum(selected)
 
+def no_zero():
+    for num in selected:
+        if num == 0:
+            return False
+    return True
+
 def get_min_sum_combo(depth):
     global ans
     if depth == n:
-        if len(selected) == n:
+        if no_zero():
             ans = min(ans, get_sum())
             return
         return
     
     for i in range(n):
-        if depth != i and not visited[i]:
-            if grid[depth][i] == 0:
-                continue              
+        if depth != i and not visited[i]:           
             selected.append(grid[depth][i])
             visited[i] = True
             get_min_sum_combo(depth + 1)
