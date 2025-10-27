cbe3db849dd005579f08f372df673c9cd07723fa
Jaehyun Kevin Lee <angle0918@naver.com>
2025-10-27 14:16:00 +0900

    Title: 수들의 합 최대화하기, Time: 101ms, Memory: 20MB, Status: Passed - Codetree

---
+++ 251027/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -0,0 +1,26 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+ans = 0
+selected = []
+visited = [False] * (n+1)
+
+def get_sum():
+    return sum(selected)
+
+def get_max_sum_combo(depth):
+    global ans
+    if depth == n:
+        ans = max(ans, get_sum())
+        return
+    
+    for i in range(n):
+        if not visited[i]:
+            selected.append(grid[depth][i])
+            visited[i] = True
+            get_max_sum_combo(depth + 1)
+            visited[i] = False
+            selected.pop()
+
+get_max_sum_combo(0)
+print(ans)
