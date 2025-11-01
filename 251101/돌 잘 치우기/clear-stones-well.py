fb0140d147698f31fce9835ac9f9f99458d84503
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-01 16:47:29 +0900

    Title: 돌 잘 치우기, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 251101/돌 잘 치우기/clear-stones-well.py
+++ 251101/돌 잘 치우기/clear-stones-well.py
@@ -1,59 +1,76 @@
 from collections import deque
-from itertools import combinations
-import sys
 
-input = sys.stdin.readline
-
-# 입력
-N, K, M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
-starts = [tuple(map(int, input().split())) for _ in range(K)]  # 1-indexed
-
-# 0-index로 변환
-starts = [(r-1, c-1) for r, c in starts]
-
-# 돌 좌표들 수집
-stones = [(r, c) for r in range(N) for c in range(N) if board[r][c] == 1]
-
-# 4방향
-DR = (1, -1, 0, 0)
-DC = (0, 0, 1, -1)
+n, num_starts, num_bomb = tuple(map(int, input().split()))
+grid = [list(map(int, input().split())) for _ in range(n)]
+start_points = []
+for _ in range(num_starts):
+    r, c = tuple(map(int, input().split()))
+    r, c = r - 1, c - 1
+    start_points.append((r, c))
 
 def in_range(r, c):
-    return 0 <= r < N and 0 <= c < N
+    return 0 <= r < n and 0 <= c < n
 
-def bfs(removed_set):
-    """removed_set: 이번에 치운 돌 좌표 집합(set of (r,c))"""
+def can_go(r, c):
+    return in_range(r, c) and not visited[r][c] and grid[r][c] == 0
+
+visited = [[0] * n for _ in range(n)]
+
+def bfs():
+    global visited
+    visited = [[0] * n for _ in range(n)]
     q = deque()
-    visited = [[False]*N for _ in range(N)]
+    for (r, c) in start_points:
+        q.append((r, c))
+    
+    xds, yds = [0, 0, -1, 1], [-1, 1, 0, 0]
 
-    # 시작점 넣기 (모두 0 위에 있다고 가정)
-    for r, c in starts:
-        if not visited[r][c]:
-            visited[r][c] = True
-            q.append((r, c))
-
-    cnt = 0
     while q:
-        r, c = q.popleft()
-        cnt += 1
-        for dr, dc in zip(DR, DC):
-            nr, nc = r + dr, c + dc
-            if not in_range(nr, nc) or visited[nr][nc]:
-                continue
+        x, y = q.popleft()
 
-            cell = board[nr][nc]
-            # 이동 가능 조건: 0이거나, 이번에 치운 돌
-            if cell == 0 or (cell == 1 and (nr, nc) in removed_set):
-                visited[nr][nc] = True
-                q.append((nr, nc))
+        for xd, yd in zip(xds, yds):
+            nx = x + xd
+            ny = y + yd
+            if can_go(nx, ny):
+                visited[nx][ny] = 1
+                q.append((nx, ny))
 
-    return cnt
-
+stones = [(i, j) for i in range(n) for j in range(n) if grid[i][j] == 1]
 ans = 0
+selected_stones = []
 
-# M == 0인 경우도 커버
-for removed in combinations(stones, M):
-    ans = max(ans, bfs(set(removed)))
+def get_stone_combo_and_min_max(selected, idx):
+    global ans
 
+    if selected == num_bomb:
+        for (x, y) in selected_stones:
+            grid[x][y] = 0
+        
+        bfs()
+        
+        sub_ans = sum(1 for i in range(n) for j in range(n) if visited[i][j])
+
+        ans = max(ans, sub_ans)
+        # if ans == sub_ans:
+            # print(ans)
+            # print(selected_stones)
+            # for row in grid:
+            #     print(*row)
+            # print("======")
+    
+        for (x, y) in selected_stones:
+            grid[x][y] = 1
+        
+        return
+    
+    if idx == len(stones):
+        return 
+
+    selected_stones.append(stones[idx])
+    get_stone_combo_and_min_max(selected + 1, idx + 1)
+
+    selected_stones.pop()
+    get_stone_combo_and_min_max(selected, idx + 1)
+
+get_stone_combo_and_min_max(0, 0)
 print(ans)
