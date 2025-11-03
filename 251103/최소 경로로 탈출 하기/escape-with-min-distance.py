5159435b951aba9fd8c268edd8571a5a8c96b5bd
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-03 09:59:33 +0900

    Title: 최소 경로로 탈출 하기, Time: 53ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251103/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
+score = [[0] * m for _ in range(n)]
+
+# Please write your code here.
+def in_range(r, c):
+    return 0 <= r < n and 0 <= c < n
+
+def can_go(r, c):
+    return in_range(r, c) and grid[r][c] and not visited[r][c]
+
+q = deque()
+drs, dcs = [0, 0, -1, 1], [-1, 1, 0, 0]
+
+def push(r, c, value):
+    score[r][c] = value
+    visited[r][c] = 1
+    q.append((r, c))
+
+def bfs():
+    while q:
+        (r, c) = q.popleft()        
+        for dr, dc in zip(drs, dcs):
+            nr, nc = r + dr, c + dc
+            if can_go(nr, nc):
+                push(nr, nc, score[r][c] + 1)
+
+push(0, 0, 0)
+bfs()
+
+print(score[n-1][m-1])
