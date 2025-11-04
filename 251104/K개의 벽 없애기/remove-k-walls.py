bc87969a133b68c8eef77807a69ab5fa4fb24edf
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-04 12:17:00 +0900

    Title: K개의 벽 없애기, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 251104/K개의 벽 없애기/remove-k-walls.py
+++ 251104/K개의 벽 없애기/remove-k-walls.py
@@ -1,53 +1,80 @@
 from collections import deque
-import sys
-
-input = sys.stdin.readline
 
 n, k = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 sr, sc = map(int, input().split())
 er, ec = map(int, input().split())
 
-sr -= 1; sc -= 1
-er -= 1; ec -= 1
+sr -= 1
+sc -= 1
+er -= 1
+ec -= 1
 
+# 벽 배열 생성
+walls = []
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1:
+            walls.append((i, j))
+
+# 선택된 벽 저장 배열 선언
+selected_wall = []
+
+# 방문 가능 여부 확인
 def in_range(r, c):
     return 0 <= r < n and 0 <= c < n
 
-# visited[r][c][b]: (r,c)에 벽 b개 부수고 도달했는가
-visited = [[[False]*(k+1) for _ in range(n)] for __ in range(n)]
+# 정답: 도달 불가 시 -1
+ans = float('inf')
 
-# (r, c, broken, dist)
-q = deque()
+# 벽 선택 이후 dfs 돌기
+def bfs():
+    q = deque()
+    visited = [[0] * n for _ in range(n)]
+    path = [[0] * n for _ in range(n)]
 
-# 시작점이 벽인 경우도 안전하게 처리 (가능하면 시작과 동시에 1개 부수고 시작)
-visited[sr][sc][0] = True
-q.append((sr, sc, 0, 0))
+    q.append((sr, sc))
+    visited[sr][sc] = 1
+    
+    drs, dcs = [0, 0, -1, 1], [1, -1, 0, 0]
 
-drs = (0, 0, -1, 1)
-dcs = (1, -1, 0, 0)
+    while q:
+        r, c = q.popleft() 
+        if (r, c) == (er, ec):
+            return path[r][c]
+        
+        for dr, dc in zip(drs, dcs):
+            nr, nc = r + dr, c + dc
+            if in_range(nr, nc) and not visited[nr][nc] and grid[nr][nc] != 1:
+                visited[nr][nc] = 1
+                path[nr][nc] = path[r][c] + 1
+                q.append((nr, nc))
+    return float('inf')
 
-ans = -1
+def select_wall_get_ans(num_wall, idx):
+    global ans
 
-while q:
-    r, c, b, d = q.popleft()
-    if (r, c) == (er, ec):
-        ans = d
-        break
+    if num_wall == k:
+        for wall in selected_wall:
+            wr, wc = wall
+            grid[wr][wc] = 0
+        
+        dist = bfs()
+        ans = min(ans, dist)
 
-    for dr, dc in zip(drs, dcs):
-        nr, nc = r + dr, c + dc
-        if not in_range(nr, nc):
-            continue
+        for wall in selected_wall:
+            wr, wc = wall
+            grid[wr][wc] = 1
+        return
+    
+    if idx == len(walls):
+        return
+    
+    selected_wall.append(walls[idx])
+    select_wall_get_ans(num_wall + 1, idx + 1)
+    selected_wall.pop()
 
-        cell = grid[nr][nc]
-        if cell == 0:
-            if not visited[nr][nc][b]:
-                visited[nr][nc][b] = True
-                q.append((nr, nc, b, d+1))
-        else:  # cell == 1, 벽
-            if b < k and not visited[nr][nc][b+1]:
-                visited[nr][nc][b+1] = True
-                q.append((nr, nc, b+1, d+1))
-
-print(ans)
+    select_wall_get_ans(num_wall, idx + 1)
+      
+select_wall_get_ans(0, 0)
+print(-1 if ans == float('inf') else ans)
