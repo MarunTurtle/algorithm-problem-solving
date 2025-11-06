56e6f61164c9731f7b576fa95d5cab65090d4a24
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-06 11:36:35 +0900

    [Bronze V] Title: 오늘 날짜, Time: 80 ms, Memory: 9272 KB -BaekjoonHub

--- node.js/백준/Bronze/10699. 오늘 날짜/오늘 날짜.js
+++ node.js/백준/Bronze/10699. 오늘 날짜/오늘 날짜.js
@@ -1,7 +1,1 @@
-let today = new Date();
-
-let year = today.getFullYear();
-let month = String(today.getMonth() + 1).padStart(2, '0');
-let date = String(today.getDate()).padStart(2, '0');
-
-console.log(`${year}-${month}-${date}`)
+console.log(new Date().toISOString().slice(0, 10));
