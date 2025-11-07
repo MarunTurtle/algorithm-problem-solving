93655ffa79c7590475269497f4833c5e96054b3f
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-07 10:50:12 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 572ms, Memory: 57MB, Status: Passed - Codetree

--- 251107/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 251107/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,34 +1,34 @@
-import sys
-sys.setrecursionlimit(10**7)
-from collections import deque  # 없어도 되지만 원래 코드 호환 위해 둠
-
+# 변수 선언 및 입력:
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
 
-# 증가하는 방향으로만 이동 → DAG → 메모이제이션으로 최장 증가 경로
-drs = [0, 0, -1, 1]
-dcs = [-1, 1, 0, 0]
-
-dp = [[0] * n for _ in range(n)]  # dp[r][c] = (r,c)에서 시작하는 최장 길이
-
-def in_range(r, c):
-    return 0 <= r < n and 0 <= c < n
-
-def dfs(r, c):
-    if dp[r][c]:                # 이미 계산됨
-        return dp[r][c]
-    best = 1                    # 자기 자신만 포함하는 길이
-    cur = grid[r][c]
-    for dr, dc in zip(drs, dcs):
-        nr, nc = r + dr, c + dc
-        if in_range(nr, nc) and grid[nr][nc] > cur:
-            best = max(best, 1 + dfs(nr, nc))
-    dp[r][c] = best
-    return best
-
+cells = []
 ans = 0
-for r in range(n):
-    for c in range(n):
-        ans = max(ans, dfs(r, c))
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+for i in range(n):
+    for j in range(n):
+        cells.append((grid[i][j], i, j))
+
+cells.sort()
+
+for i in range(n):
+    for j in range(n):
+        dp[i][j] = 1
+
+for _, x, y in cells:
+    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and grid[nx][ny] > grid[x][y]:
+            dp[nx][ny] = max(dp[nx][ny], dp[x][y] + 1)
+
+for i in range(n):
+    for j in range(n):
+        ans = max(ans, dp[i][j])
 
 print(ans)
