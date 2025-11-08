d878cc5eac1fcc15db47899a893cb85aa0f57db6
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-08 14:41:25 +0900

    Title: 최대 증가 부분 수열, Time: 56ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251108/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 251108/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -1,23 +1,13 @@
-import sys
-
-INT_MIN = -sys.maxsize
-
 n = int(input())
 nums = list(map(int, input().split()))
 
 dp = [0] * n
 
-def initialize():
-    for i in range(n):
-        dp[i] = 1
+dp[0] = 1
 
-initialize()
-
-for i in range(1, n):
+for i in range(n):
     for j in range(i):
-
         if nums[j] < nums[i]:
             dp[i] = max(dp[i], dp[j] + 1)
-
 
 print(max(dp))
