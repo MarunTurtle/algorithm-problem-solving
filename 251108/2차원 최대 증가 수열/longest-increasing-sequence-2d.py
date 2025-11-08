8c6a04f0dbf22f2e196df9374e9227125f2e16ee
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-08 20:54:21 +0900

    Title: 2차원 최대 증가 수열, Time: 75ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251108/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -0,0 +1,34 @@
+import sys
+
+INT_MIN = -sys.maxsize
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+dp = [[0] * m for _ in range(n)]
+
+def initialize():
+    for i in range(n):
+        for j in range(m):
+            dp[i][j] = INT_MIN
+    
+    dp[0][0] = 1
+
+initialize()
+
+def can_go(r, c, nr, nc):
+    return r > nr and c > nc
+
+for r in range(1, n):
+    for c in range(1, n):
+        for nr in range(r):
+            for nc in range(c):                
+                if dp[nr][nc] == INT_MIN:
+                    continue
+                
+                if can_go(r, c, nr, nc):
+                    if grid[r][c] > grid[nr][nc]:
+                        dp[r][c] = max(dp[r][c], dp[nr][nc] + 1)
+
+
+print(max(map(max, dp)))
