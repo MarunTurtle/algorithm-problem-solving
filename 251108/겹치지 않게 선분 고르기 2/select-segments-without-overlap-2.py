e942ff3623d4f07c7bb0ec3c9193d373aff6d71d
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-08 21:18:42 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 56ms, Memory: 16MB, Status: Passed - Codetree

--- 251108/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 251108/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -2,11 +2,21 @@ n = int(input())
 lines = list(tuple(map(int, input().split())) for _ in range(n))
 lines.sort(key = lambda x : (x[1], x[0]))
 
-dp = [1] * n
+# dp = [1] * n
 
-for i in range(n):
-    for j in range(i):
-        if lines[j][1] < lines[i][0]:
-            dp[i] = max(dp[i], dp[j] + 1)
+# for i in range(n):
+#     for j in range(i):
+#         if lines[j][1] < lines[i][0]:
+#             dp[i] = max(dp[i], dp[j] + 1)
 
-print(max(dp))
+# print(max(dp))
+
+cnt = 0
+last_end = -1
+
+for s, e in lines:
+    if s > last_end:
+        cnt += 1
+        last_end = e
+
+print(cnt)
