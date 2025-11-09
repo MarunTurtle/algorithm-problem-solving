acdb8d6ba11994a530f1bfa5bdb0274b40d646e2
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-09 14:46:51 +0900

    Title: 부분 수열의 합이 M, Time: 95ms, Memory: 18MB, Status: Passed - Codetree

--- 251109/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
+++ 251109/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
@@ -4,10 +4,10 @@ dp = [float('inf')] * (target + 1)
 dp[0] = 0
 
 for i in range(1, n+1):
-    for j in range(target, 0, -1):
-        if dp[j - arr[i]] == float('inf'):
-            continue
+    for j in range(target, -1, -1):
         if j >= arr[i]:
+            if dp[j - arr[i]] == float('inf'):
+                continue
             dp[j] = min(dp[j], dp[j - arr[i]] + 1)
 
 print(-1 if dp[-1] == float('inf') else dp[-1])
