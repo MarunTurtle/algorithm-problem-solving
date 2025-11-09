9b65209fbf43e8d71b64dd58967e7ac243fafafc
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-09 14:28:07 +0900

    Title: 동전 거슬러주기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251109/동전 거슬러주기/coin-change.py
+++ 251109/동전 거슬러주기/coin-change.py
@@ -3,8 +3,6 @@ n, target = map(int, input().split())
 coins = [0] + list(map(int, input().split()))
 
 dp = [0] + [float('inf')] * target
-for coin in coins:
-    dp[coin] = 1
 
 for v in range(1, target + 1):
     for j in range(1, n + 1):
@@ -12,5 +10,5 @@ for v in range(1, target + 1):
             if dp[v - coins[j]] == float('inf'):
                 continue
             dp[v] = min(dp[v], dp[v - coins[j]] + 1)
-            
+
 print(dp[-1])
