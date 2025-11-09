8ff5bdc907bf0fbca2771bf26361e6f97ce7f528
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-09 14:42:15 +0900

    Title: 부분 수열의 합이 M, Time: 84ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251109/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
@@ -0,0 +1,13 @@
+n, target = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+dp = [float('inf')] * (target + 1)
+dp[0] = 0
+
+for i in range(1, n+1):
+    for j in range(target, -1, -1):
+        if dp[j - arr[i]] == float('inf'):
+            continue
+        if j >= arr[i]:
+            dp[j] = min(dp[j], dp[j - arr[i]] + 1)
+
+print(-1 if dp[-1] == float('inf') else dp[-1])
