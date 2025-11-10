9e2f736022567a1430799546cd8ae86f3c580092
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-10 15:28:24 +0900

    Title: 최소 차 분할, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 251110/최소 차 분할/minimum-diff-partition.py
+++ 251110/최소 차 분할/minimum-diff-partition.py
@@ -1,16 +1,14 @@
 n = int(input())
-arr = [0] + list(map(int, input().split()))
+arr = list(map(int, input().split()))
 m = sum(arr)
-dp = [float('-inf')] * (m + 1)
-dp[0] = 0
-diff = [float('inf')] * (m+1)
-max_value = 0
 
-for j in range(1, n+1):
-    for i in range(m, -1, -1):
-        if i >= arr[j]:
-            if dp[i - arr[j]] != float('-inf'):
-                dp[i] = max(dp[i], dp[i - arr[j]] + 1)
-                diff[i] = min(diff[i], abs(i * 2 - m))
+reachable = [False] * (m + 1)
+reachable[0] = True
 
-print(min(diff))
+for a in arr:
+    for s in range(m, a - 1, -1):
+        if reachable[s - a]:
+            reachable[s] = True
+
+ans = min(abs(m - 2*s) for s in range(1, m) if reachable[s])
+print(ans)
