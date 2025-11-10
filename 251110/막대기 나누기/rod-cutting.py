2ef40efbc6ddca6a452ab330fd4a32c84f98133c
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-10 14:56:04 +0900

    Title: 막대기 나누기, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 251110/막대기 나누기/rod-cutting.py
+++ 251110/막대기 나누기/rod-cutting.py
@@ -4,10 +4,9 @@ dp = [float('-inf')] * (n+1)
 dp[0] = 0
 
 for i in range(1, n+1): # 총 막대 길이
-    for j in range(1, n+1): # 선택한 막대 
-        if i >= j: 
-            if dp[i - j] != float('-inf'):
-                dp[i] = max(dp[i], dp[i-j] + profit[j])
+    for j in range(1, i+1): # 선택한 막대 
+        if dp[i - j] != float('-inf'):
+            dp[i] = max(dp[i], dp[i-j] + profit[j])
 
 print(max(dp))
             
