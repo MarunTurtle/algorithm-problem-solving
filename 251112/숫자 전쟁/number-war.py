b15293805edde091bd3bc045cd295b0b1f9d5340
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-12 23:14:21 +0900

    Title: 숫자 전쟁, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251112/숫자 전쟁/number-war.py
+++ 251112/숫자 전쟁/number-war.py
@@ -1,17 +1,22 @@
 n = int(input())
-first_cards = [0] + list(map(int, input().split()))
-second_cards = [0] + list(map(int, input().split()))
-dp = [[0] * (n+1) for _ in range(n+1)]
+left  = list(map(int, input().split()))
+right = list(map(int, input().split()))
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if first_cards[i] > second_cards[j]:    # win
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + second_cards[j]
-        else:                                   # lose, draw
-            dp[i][j] = 0
+dp = [[0]*(n+1) for _ in range(n+1)]
 
+# 뒤에서 앞으로 채우기
+for i in range(n-1, -1, -1):
+    for j in range(n-1, -1, -1):
+        # 1) 왼쪽 버리기
+        best = dp[i+1][j]
+        # 2) 둘 다 버리기
+        if dp[i+1][j+1] > best:
+            best = dp[i+1][j+1]
+        # 3) 왼쪽 > 오른쪽이면, 오른쪽만 버리고 점수 획득
+        if left[i] > right[j]:
+            cand = dp[i][j+1] + right[j]
+            if cand > best:
+                best = cand
+        dp[i][j] = best
 
-ans = 0
-for i in range(1, n+1):
-    ans = max(ans, dp[i][n])
-print(ans)
+print(dp[0][0])
