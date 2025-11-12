1becd2095c8e437ab21b4dc029cd089784fb179f
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-12 13:46:53 +0900

    Title: 계단 오르기 2, Time: 52ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251112/계단 오르기 2/climbing-stairs-2.py
@@ -0,0 +1,15 @@
+n = int(input())
+coin = [0] + list(map(int, input().split()))
+dp = [[0 for _ in range(4)] for _ in range(n + 1)]
+
+dp[1][1] = coin[1]
+dp[2][0] = coin[2]
+
+for i in range(2, n+1):
+    for k in range(4):
+        if dp[i-2][k] != 0:
+            dp[i][k] = max(dp[i][k], dp[i-2][k] + coin[i])
+        if k and dp[i-1][k-1] != 0:
+            dp[i][k] = max(dp[i][k], dp[i-1][k-1] + coin[i])
+
+print(max(map(max, dp)))
