28f581744fa77000785905eb1a57a398956b24fd
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-13 15:56:56 +0900

    Title: 숫자 전쟁, Time: 105ms, Memory: 20MB, Status: Wrong Answer - Codetree

--- 251113/숫자 전쟁/number-war.py
+++ 251113/숫자 전쟁/number-war.py
@@ -8,17 +8,17 @@ for i in range(1, n+1):
         if dp[i][j] == float('-inf'):
             continue
         value = second_cards[j] if first_cards[i] > second_cards[j] else 0
-        # print(value)
+        # print(f"value: {value}")
         dp[i][j] = max(max(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + value
-
+        # print(f"dp[{i}][{j}]: {dp[i][j]}")
         if first_cards[i] > second_cards[j]:
             for k in range(i+1, n+1):
                 dp[k][j] = float('-inf')
         else:
-            for k in range(i+1, n+1):
+            for k in range(max(j+1, i+1), n+1):
                 dp[i][k] = float('-inf')
 
         # for row in dp:
         #     print(*row)
 
-print(max(map(max, dp)))
+print(dp[-1][-1])
