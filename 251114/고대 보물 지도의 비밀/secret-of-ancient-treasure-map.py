e54e86ba7f2a8d31bed161e9bc3526997b5154fe
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-14 18:03:49 +0900

    Title: 고대 보물 지도의 비밀, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251114/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 251114/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -3,20 +3,24 @@ numbers = [0] + list(map(int, input().split()))
 dp = [[float('-inf')] * (K+1) for _ in range(n+1)]
 dp[0][0] = 0
 
+max_ans = float('-inf')
+
 for i in range(1, n+1):
     for k in range(K+1):
         if numbers[i] >= 0:
             if dp[i-1][k] == float('-inf'):
-                dp[i][k] = 0
+                continue
             else:
                 dp[i][k] = dp[i-1][k] + numbers[i]
+                max_ans = max(max_ans, dp[i][k])
         else:
             if k == 0:
                 dp[i][k] = 0
             else:
                 dp[i][k] = dp[i-1][k-1] + numbers[i]
-
+                max_ans = max(max_ans, dp[i][k])
+     
 # for row in dp:
 #     print(*row)
 
-print(max(map(max, dp)))
+print(max_ans)
