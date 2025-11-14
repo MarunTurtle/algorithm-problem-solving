7df2ee194291ce4ca14dea287a8a11505c0faf07
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-14 17:52:55 +0900

    Title: 고대 보물 지도의 비밀, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251114/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 251114/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -1,12 +1,12 @@
 n, K = map(int, input().split())
 numbers = [0] + list(map(int, input().split()))
-dp = [[-1] * (K+1) for _ in range(n+1)]
+dp = [[float('-inf')] * (K+1) for _ in range(n+1)]
 dp[0][0] = 0
 
 for i in range(1, n+1):
     for k in range(K+1):
         if numbers[i] >= 0:
-            if dp[i-1][k] == -1:
+            if dp[i-1][k] == float('-inf'):
                 dp[i][k] = 0
             else:
                 dp[i][k] = dp[i-1][k] + numbers[i]
@@ -15,5 +15,8 @@ for i in range(1, n+1):
                 dp[i][k] = 0
             else:
                 dp[i][k] = dp[i-1][k-1] + numbers[i]
+
+# for row in dp:
+#     print(*row)
 
 print(max(map(max, dp)))
