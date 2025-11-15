6cc218688be86afc8bf5bb0205a470a0e0aa997c
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-15 16:09:58 +0900

    Title: 둘 중 하나 잘 고르기, Time: 65ms, Memory: 17MB, Status: Passed - Codetree

--- 251115/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 251115/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -1,11 +1,32 @@
-n = int(input())
-red = []
-blue = []
+INF = -10**18
 
+n = int(input())
+cards = []
 for _ in range(2 * n):
     r, b = map(int, input().split())
-    red.append(r)
-    blue.append(b)
+    cards.append((r, b))
 
-dp = [[-1, -1, -1] for _ in range(2 * n + 1)]
-print(dp)
+N = 2 * n
+# dp[i][r] = i번째 카드까지 고려했을 때,
+#           빨강을 r번 쓴 경우의 최대 점수
+dp = [[INF] * (n + 1) for _ in range(N + 1)]
+dp[0][0] = 0
+
+for i in range(1, N + 1):
+    r_val, b_val = cards[i - 1]
+    for red in range(0, n + 1):
+        blue = i - red
+        # 파랑 개수도 0..n 범위 안이어야 함
+        if blue < 0 or blue > n:
+            continue
+
+        # 이번 카드에서 빨강 선택
+        if red > 0 and dp[i-1][red-1] != INF:
+            dp[i][red] = max(dp[i][red], dp[i-1][red-1] + r_val)
+
+        # 이번 카드에서 파랑 선택
+        if blue > 0 and dp[i-1][red] != INF:
+            dp[i][red] = max(dp[i][red], dp[i-1][red] + b_val)
+
+ans = dp[N][n]
+print(ans)
