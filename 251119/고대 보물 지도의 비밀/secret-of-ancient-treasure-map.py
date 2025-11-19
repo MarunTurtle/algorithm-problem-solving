fd2ae05840c254e1a562c8a374a9b13e02b4efd9
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-19 17:05:58 +0900

    Title: 고대 보물 지도의 비밀, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251119/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 251119/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -4,12 +4,11 @@ n, k = map(int, input().split())
 nums = [0] + list(map(int, input().split()))
 
 grid = [[INT_MIN] * (k+1) for _ in range(n+1)]
-grid[0][0] = 0
 
 # i번째 라운드에서 음수 카드가 j개 일 때
 for i in range(1, n+1):
     for j in range(k+1):
-        if nums[i] > 0:
+        if nums[i] >= 0:
             if grid[i-1][j] != INT_MIN:
                 grid[i][j] = max(nums[i], grid[i-1][j] + nums[i])
             else:
