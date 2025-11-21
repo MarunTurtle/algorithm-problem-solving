062263921bfc263607ada3dc60cc1330f1dc111f
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-21 11:17:01 +0900

    Title: 올바른 등식 만들기, Time: 348ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 251121/올바른 등식 만들기/right-equality.py
+++ 251121/올바른 등식 만들기/right-equality.py
@@ -1,29 +1,39 @@
-import sys
-sys.setrecursionlimit(10**7)
-
-N, M = map(int, input().split())
-nums = list(map(int, input().split()))
-
+n, m = map(int, input().split())
+nums = [0] + list(map(int, input().split()))
 ans = 0
+dp = [[[] for _ in range(n+1)] for _ in range(n+1)]
 
-def dfs(idx, value):
-    global ans
-    
-    if value > 20 or value < -20:
-        return
-    
-    if idx == N:
-        if value == M:
-            ans += 1 
-        return
-    
-    if sum(nums[idx:]) == 0:
-        ans = 2**len(nums)
-        return
+dp[0][0].append(0)
 
-    dfs(idx + 1, value + nums[idx])
-    dfs(idx + 1, value - nums[idx])
+def in_range(num):
+    return -20 <= num <= 20
 
+for i in range(1, n+1):
+    v = nums[i]
+    if len(dp[i-1][0]) != 0 and in_range(dp[i-1][0][0] + v):
+        dp[i][0].append(dp[i-1][0][0] + v)
 
-dfs(0, 0)
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        v = nums[i]
+        if len(dp[i-1][j-1]) != 0:
+            for e in dp[i-1][j-1]:
+                r = e - v
+                if in_range(r):
+                    dp[i][j].append(r)
+        if len(dp[i-1][j]) != 0:
+            for e in dp[i-1][j]:
+                r = e + v
+                if in_range(r):
+                    dp[i][j].append(r)
+
+for col in dp[n]:
+    if len(col) != 0:
+        for e in col:
+            if e == m:
+                ans += 1 
+
+# for row in dp:
+#     print(*row)
+
 print(ans)
