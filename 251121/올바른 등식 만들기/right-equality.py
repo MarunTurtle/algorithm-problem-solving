36410296fcc1d38f8736af496988de5fd6ffdee6
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-21 10:29:49 +0900

    Title: 올바른 등식 만들기, Time: 1074ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

---
+++ 251121/올바른 등식 만들기/right-equality.py
@@ -0,0 +1,25 @@
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+
+visited = [0] * N
+ans = 0
+
+def dfs(idx, value):
+    global ans
+    # print(f"idx: {idx} || value: {value} || {visited}")
+    if idx == N:
+        if value == M:
+            ans += 1 
+        return
+    
+    if value > 20 or value < -20:
+        return
+    
+    if visited[idx] == 0:
+        visited[idx] = 1
+        dfs(idx + 1, value + nums[idx])
+        dfs(idx + 1, value - nums[idx])
+        visited[idx] = 0
+
+dfs(0, 0)
+print(ans)
