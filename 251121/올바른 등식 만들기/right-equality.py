bd9b77e31b2158c54bcd959d564cb00d61a0d5ae
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-21 11:39:11 +0900

    Title: 올바른 등식 만들기, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 251121/올바른 등식 만들기/right-equality.py
+++ 251121/올바른 등식 만들기/right-equality.py
@@ -1,39 +1,28 @@
 n, m = map(int, input().split())
 nums = [0] + list(map(int, input().split()))
 ans = 0
-dp = [[[] for _ in range(n+1)] for _ in range(n+1)]
+RANGE = 41 # -20 <-> 20
 
-dp[0][0].append(0)
+dp = [[0] * RANGE for _ in range(n+1)]
+dp[0][20] = 1
 
 def in_range(num):
-    return -20 <= num <= 20
+    return 0 <= num <= 40
 
 for i in range(1, n+1):
     v = nums[i]
-    if len(dp[i-1][0]) != 0 and in_range(dp[i-1][0][0] + v):
-        dp[i][0].append(dp[i-1][0][0] + v)
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        v = nums[i]
-        if len(dp[i-1][j-1]) != 0:
-            for e in dp[i-1][j-1]:
-                r = e - v
-                if in_range(r):
-                    dp[i][j].append(r)
-        if len(dp[i-1][j]) != 0:
-            for e in dp[i-1][j]:
-                r = e + v
-                if in_range(r):
-                    dp[i][j].append(r)
+    for e in range(RANGE):
+        if dp[i-1][e] == 0:
+            continue
+        cnt = dp[i-1][e]
+        
+        plus_v = e + v
+        if in_range(plus_v):
+            dp[i][plus_v] += cnt
 
-for col in dp[n]:
-    if len(col) != 0:
-        for e in col:
-            if e == m:
-                ans += 1 
-
-# for row in dp:
-#     print(*row)
-
-print(ans)
+        minus_v = e - v
+        if in_range(minus_v):
+            dp[i][minus_v] += cnt
+    
+print(dp[n][m+20])
