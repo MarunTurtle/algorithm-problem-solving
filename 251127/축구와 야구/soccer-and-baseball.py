5ec951f219a9ea4807e49f1f5318da37ddd3572d
Jaehyun Kevin Lee <angle0918@naver.com>
2025-11-27 14:37:44 +0900

    Title: 축구와 야구, Time: 74ms, Memory: 18MB, Status: Passed - Codetree

--- 251127/축구와 야구/soccer-and-baseball.py
+++ 251127/축구와 야구/soccer-and-baseball.py
@@ -1,31 +1,23 @@
-from itertools import combinations
 import sys
 
-INT_MIN = -sys.maxsize
+input = sys.stdin.readline
+INT_MIN = -10**18  # 충분히 작은 음수
 
-n = int(input())
-students = [list(map(int, input().split())) for _ in range(n)]
+n = int(input().strip())
+students = [tuple(map(int, input().split())) for _ in range(n)]
 
-students.sort(key=lambda x: (x[0], x[1]))
+dp = [[INT_MIN] * 10 for _ in range(12)]
+dp[0][0] = 0
 
-combo = students[:20]
+for s, b in students:
+    for i in range(11, -1, -1):
+        for j in range(9, -1, -1):
+            if dp[i][j] == INT_MIN:
+                continue
+            if i + 1 <= 11:
+                dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + s)
+            if j + 1 <= 9:
+                dp[i][j + 1] = max(dp[i][j + 1], dp[i][j] + b)
 
-max_sum = 0
-
-dp = [[[0] * 10 for _ in range(12)]for _ in range(21)]
-dp[0][0][0] = 0
-dp[1][1][0] = combo[0][0]
-dp[1][0][1] = combo[0][1]
-
-for r in range(1, 20):
-    for i in range(12):
-        for j in range(10):
-            if dp[r][i][j] != 0:
-                if i+1 < 12:
-                    dp[r+1][i+1][j] = max(dp[r+1][i+1][j], dp[r][i][j] + combo[r][0])
-                if j+1 < 10:
-                    dp[r+1][i][j+1] = max(dp[r+1][i][j+1], dp[r][i][j] + combo[r][1])
-
-max_sum = max(max_sum, dp[20][11][9])
-
-print(max_sum)
+ans = dp[11][9]
+print(ans)
